1d1615c1274bb381b14e42f62b5ebbd2aca113ce
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-10 12:40:23 -0800

    Create Introduction to Data Engineering.py

---
+++ Introduction to Data Engineering/Introduction to Data Engineering.py
@@ -0,0 +1,14 @@
+""""******************************************************************************
+ Explore the differences between a data engineer
+and a data scientist, get an overview of the various tools data engineers use and 
+expand your understanding of how cloud technology plays a role in data engineering....
+**************************************************************************************"""
+"""
+  {Data Engineer} 
+---What they Do ? :
+  - gathered data from different sources
+  - optimize database for analysis
+  - remove corrupt data
+---Definition Of the Job :
+    Develops, construct, test & mantain architectures such as data-bases/ large scale processing systems
+"""
