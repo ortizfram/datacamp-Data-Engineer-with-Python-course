362cb0a76bc9e6ddf0a94881da5f0eadad3ed2c6
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-10 12:59:22 -0800

    Update Introduction to Data Engineering.py

--- Introduction to Data Engineering/Introduction to Data Engineering.py
+++ Introduction to Data Engineering/Introduction to Data Engineering.py
@@ -3,12 +3,32 @@
 and a data scientist, get an overview of the various tools data engineers use and 
 expand your understanding of how cloud technology plays a role in data engineering....
 **************************************************************************************"""
-"""
-  {Data Engineer} 
----What they Do ? :
-  - gathered data from different sources
-  - optimize database for analysis
-  - remove corrupt data
----Definition Of the Job :
-    Develops, construct, test & mantain architectures such as data-bases/ large scale processing systems
-"""
+#///WHAT IS DATA ENGINEERING///
+#Tasks of the data engineer
+"""find best fit
+Possible Answers
+
+    1 Apply a statistical model to a large dataset to find outliers.
+OK  2 Set up scheduled ingestion of data from the application databases to an analytical database.
+    3 Come up with a database schema for an application.                                     """
+#---
+#Data engineer or data scientist?
+"""drag items into correct bucket
+
+{Data engineer}
+    clean corrupt data
+    develop scalable data architecture
+    set up processes to bring data together
+    streamline data acquisition 
+    cloud tech                                              """
+#---
+#Data engineering problems
+"""decide where you're best suited to be of help
+
+ok  1 Data scientists are querying the online store databases directly and slowing down the functioning of the application since it's using the same database.
+    2 Harmful product recommendations are affecting the sales numbers of the online store.
+    3 The online store is slow because the application's database server doesn't have enough memory.
+    
+(data engineer should make sure there's a separate database for analytics.)                            """
+#---
+#///TOOLS///
