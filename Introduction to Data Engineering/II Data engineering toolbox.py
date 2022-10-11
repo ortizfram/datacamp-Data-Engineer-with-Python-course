6a2a28fb2189dd02ceb107298ab94f3281b371d0
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-11 09:02:13 -0300

    Create II Data engineering toolbox.py

---
+++ Introduction to Data Engineering/II Data engineering toolbox.py
@@ -0,0 +1,5 @@
+"""***********************************************************************************************************
+explore the data engineer's toolbox! Learn in detail about different types of databases data engineers use,
+how parallel computing is a cornerstone of the data engineer's toolkit, and how to schedule data processing
+jobs using scheduling frameworks.
+**************************************************************************************************************"""
