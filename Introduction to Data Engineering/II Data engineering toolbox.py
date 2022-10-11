d360b38fff11609c53ab33c0df0b3c55cee1a462
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-11 11:26:47 -0300

    Update II Data engineering toolbox.py

--- Introduction to Data Engineering/II Data engineering toolbox.py
+++ Introduction to Data Engineering/II Data engineering toolbox.py
@@ -116,3 +116,24 @@ print(athlete_events_dask.groupby('Year').Age.mean().compute())
       Built from need to use structures queries for pararell processing
       Initially used Hadoop MapReduce                                                        """                                                                                                                                                                                                                                                         
       
+#---
+#A PySpark groupby
+"""The methods you're going to use in this exercise are:
+            .printSchema(): helps print the schema of a Spark DataFrame.
+            .groupBy(): grouping statement for an aggregation.
+            .mean(): take the mean over each group.
+            .show(): show the results."""
+# Print the type of athlete_events_spark
+print(type(athlete_events_spark))
+
+# Print the schema of athlete_events_spark
+print(athlete_events_spark.printSchema())
+
+# Group by the Year, and find the mean Age
+print(athlete_events_spark.groupBy('Year').mean('Age'))
+
+# Group by the Year, and find the mean Age
+print(athlete_events_spark.groupBy('Year').mean('Age').show())
+
+#---
+#
