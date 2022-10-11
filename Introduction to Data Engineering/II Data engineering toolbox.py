18315deb3ff43b972825518f7113e36d7d748e9f
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-11 13:42:08 -0800

    Update II Data engineering toolbox.py

--- Introduction to Data Engineering/II Data engineering toolbox.py
+++ Introduction to Data Engineering/II Data engineering toolbox.py
@@ -159,11 +159,34 @@ spark-submit \
 
 
 #---
-#Airflow DAGs
+#Airflow DAGs 1
 """{In Airflow, a pipeline} :
 is represented as a Directed Acyclic Graph or DAG.
 The nodes of the graph represent tasks that are executed. The directed connections
 between nodes represent dependencies between the tasks.
 *
 First, the DAG needs to run on every hour at minute 0. Fill in the schedule_interval keyword
-argument using the crontab notation. For example, every hour at minute N would be N * * * *. Remember, you need to run at minute 0.
+argument using the crontab notation. For example, every hour at minute N would be N * * * *. Remember, you need to run at minute 0."""
+# Create the DAG object
+dag = DAG(dag_id="car_factory_simulation",
+          default_args={"owner": "airflow","start_date": airflow.utils.dates.days_ago(2)},
+          #run at minute 0
+          schedule_interval="0 * * * *")
+
+#---
+#Airflow DAGs 2
+## Create the DAG object
+dag = DAG(dag_id="car_factory_simulation",
+          default_args={"owner": "airflow","start_date": airflow.utils.dates.days_ago(2)},
+          schedule_interval="0 * * * *")
+
+# Task definitions
+assemble_frame = BashOperator(task_id="assemble_frame", bash_command='echo "Assembling frame"', dag=dag)
+place_tires = BashOperator(task_id="place_tires", bash_command='echo "Placing tires"', dag=dag)
+assemble_body = BashOperator(task_id="assemble_body", bash_command='echo "Assembling body"', dag=dag)
+apply_paint = BashOperator(task_id="apply_paint", bash_command='echo "Applying paint"', dag=dag)
+
+# Complete the downstream flow
+assemble_frame.set_downstream(place_tires)
+assemble_frame.set_downstream(assemble_body)
+assemble_body.set_downstream(apply_paint)
