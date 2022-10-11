ca46b929bf02aa5cf5f6468fb3bf168f4e40e952
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-11 13:43:50 -0800

    Create III Extract, Transform and Load (ETL).py

---
+++ Introduction to Data Engineering/III Extract, Transform and Load (ETL).py
@@ -0,0 +1,4 @@
+"""**********************************************************************************************************************
+With ETL, you will learn how to extract raw data from various sources, transform this raw data into actionable insights, 
+and load it into relevant databases ready for consumption!
+*************************************************************************************************************************"""
