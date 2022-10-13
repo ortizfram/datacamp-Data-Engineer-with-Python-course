f11ca3b1e29a3d04af3a07f1cf662985b81644e2
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-13 11:51:12 -0300

    Update IV Case Study: DataCamp Ratings.py

--- Introduction to Data Engineering/IV Case Study: DataCamp Ratings.py
+++ Introduction to Data Engineering/IV Case Study: DataCamp Ratings.py
@@ -18,3 +18,37 @@ raw course rating data, into actionable course recommendations for DataCamp stud
 """You'll get the rating data for three sample users
 and then use a predefined helper function,
 print_user_comparison(), to compare the sets of course ids these users rated."""
+
+# Complete the connection URI () #(postgresql)
+connection_uri = "postgresql://repl:password@localhost:5432/datacamp_application" #database_name last /
+#create engine
+db_engine = sqlalchemy.create_engine(connection_uri) 
+
+
+# Get user with id 4387 #(pd.read_sql)
+user1 = pd.read_sql("SELECT * FROM rating WHERE user_id = 4387", db_engine)
+
+# Get user with id 18163
+user2 = pd.read_sql("SELECT * FROM rating WHERE user_id = 18163", db_engine)
+
+# Get user with id 8770
+user3 = pd.read_sql("SELECT * FROM rating WHERE user_id = 8770", db_engine)
+
+# Use the helper function to compare the 3 users
+print_user_comparison(user1, user2, user3)
+"""output:
+Course id overlap between users:
+================================
+User 1 and User 2 overlap: {32, 96, 36, 6, 7, 44, 95}
+User 1 and User 3 overlap: set()
+User 2 and User 3 overlap: set()"""
+
+
+#--- Average rating per course
+"""In this exercise, you'll complete a transformation function transform_avg_rating() 
+that aggregates the rating data using the pandas DataFrame's .groupby() method.
+The goal is to get a DataFrame with two columns, a course id and its average rating
+
+Complete this transformation function, and apply it on raw rating data extracted via
+the helper function extract_rating_data() which extracts course ratings from the rating table."""
+
