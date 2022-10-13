39b39a4b4aa1b0601088bba85764457e0e29f1ec
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-13 10:34:45 -0300

    Create IV Case Study: DataCamp Ratings.py

---
+++ Introduction to Data Engineering/IV Case Study: DataCamp Ratings.py
@@ -0,0 +1,5 @@
+"""***************************************************************************************
+Cap off all that you've learned in the previous three chapters by completing a real-world data
+engineering use case from DataCamp! You will perform and schedule an ETL process that transforms
+raw course rating data, into actionable course recommendations for DataCamp students!
+******************************************************************************************"""
