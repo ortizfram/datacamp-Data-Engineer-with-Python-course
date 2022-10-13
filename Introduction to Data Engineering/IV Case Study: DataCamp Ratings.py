1ec60be7aff916c1e517a8d7ffce2f41abd2e3c4
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-13 12:53:03 -0300

    Update IV Case Study: DataCamp Ratings.py

--- Introduction to Data Engineering/IV Case Study: DataCamp Ratings.py
+++ Introduction to Data Engineering/IV Case Study: DataCamp Ratings.py
@@ -82,3 +82,23 @@ transformed = transform_fill_programming_language(course_data)
 
 # Print out the number of missing values per column of transformed
 print(transformed.isnull().sum())
+#`````````````````````````````````````````````````````````````````````````````````````````````
+#--- recommender transformation
+"""to produce the final recommendations, you will use the average course ratings, and the list of eligible
+recommendations per user, stored in avg_course_ratings and courses_to_recommend respectively. You will do this 
+by completing the transform_recommendations() function which merges both DataFrames and finds the top 3 highest rated courses to recommend per user."""
+
+# Complete the transformation function
+def transform_recommendations(avg_course_ratings, courses_to_recommend):
+    # Merge both DataFrames
+    merged = courses_to_recommend.merge(avg_course_ratings) 
+    # Sort values by rating and group by user_id
+    grouped = merged.sort_values("rating", ascending=False).groupby("user_id")
+    # Produce the top 3 values and sort by user_id
+    recommendations = grouped.head(3).sort_values("user_id").reset_index()
+    final_recommendations = recommendations[["user_id", "course_id","rating"]]
+    # Return final recommendations
+    return final_recommendations
+
+# Use the function with the predefined DataFrame objects
+recommendations = transform_recommendations(avg_course_ratings, courses_to_recommend)
