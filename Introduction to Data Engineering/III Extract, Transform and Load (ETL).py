85fec1927f2adc112eb294d1ef2d7d569f0dcde1
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-13 09:52:00 -0300

    Update III Extract, Transform and Load (ETL).py

--- Introduction to Data Engineering/III Extract, Transform and Load (ETL).py
+++ Introduction to Data Engineering/III Extract, Transform and Load (ETL).py
@@ -144,3 +144,22 @@ film_pdf_joined.to_sql("film", db_engine_dwh, schema="store", if_exists="replace
 
 # Run the query to fetch the data
 pd.read_sql("SELECT film_id, recommended_film_ids FROM store.film", db_engine_dwh)
+
+
+
+#///////////////////PUT ALL TOGETHER///////////////////////////////////////////////
+#--- Defining a DAG
+# Define the ETL function
+def etl():
+    film_df = extract_film_to_pandas()
+    film_df = transform_rental_rate(film_df)
+    load_dataframe_to_film(film_df)
+
+# Define the ETL task using PythonOperator
+etl_task = PythonOperator(task_id='etl_film',
+                          python_callable=etl,
+                          dag=dag)
+
+# Set the upstream to wait_for_table and sample run etl()
+etl_task.set_upstream(wait_for_table)
+etl()
