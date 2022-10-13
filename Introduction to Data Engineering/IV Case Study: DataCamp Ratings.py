448e4fdf7054017242716b29cf4bab8109488b60
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-13 11:08:26 -0300

    Update IV Case Study: DataCamp Ratings.py

--- Introduction to Data Engineering/IV Case Study: DataCamp Ratings.py
+++ Introduction to Data Engineering/IV Case Study: DataCamp Ratings.py
@@ -3,3 +3,18 @@ Cap off all that you've learned in the previous three chapters by completing a r
 engineering use case from DataCamp! You will perform and schedule an ETL process that transforms
 raw course rating data, into actionable course recommendations for DataCamp students!
 ******************************************************************************************"""
+
+#--- Exploring the schema
+"""Have a look at the diagram of the database schema of datacamp_application:
+- Which column forms the relationship between the two tables?
+
+      1 The user_id column.
+      2 There is no relationship.
+  ok  3 The course_id column.
+      4 The combination of user_id and course_id columns."""
+
+
+#--- Querying the table
+"""You'll get the rating data for three sample users
+and then use a predefined helper function,
+print_user_comparison(), to compare the sets of course ids these users rated."""
