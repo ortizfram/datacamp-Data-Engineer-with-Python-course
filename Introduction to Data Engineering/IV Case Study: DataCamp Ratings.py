15b7021556fc5a154a7bbfe68a8607b0d1538265
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-13 11:54:53 -0300

    Update IV Case Study: DataCamp Ratings.py

--- Introduction to Data Engineering/IV Case Study: DataCamp Ratings.py
+++ Introduction to Data Engineering/IV Case Study: DataCamp Ratings.py
@@ -52,3 +52,20 @@ The goal is to get a DataFrame with two columns, a course id and its average rat
 Complete this transformation function, and apply it on raw rating data extracted via
 the helper function extract_rating_data() which extracts course ratings from the rating table."""
 
+# Complete the transformation function
+def transform_avg_rating(rating_data):
+    # Group by course_id and extract average rating per course
+    avg_rating = rating_data.groupby('course_id').rating.mean()
+    # Return sorted average ratings per course
+    sort_rating = avg_rating.sort_values(ascending=False).reset_index()
+    return sort_rating
+
+# Extract the rating data into a DataFrame    
+rating_data = extract_rating_data(db_engines)
+
+# Use transform_avg_rating on the extracted data and print results
+avg_rating_data = transform_avg_rating(rating_data)
+print(avg_rating_data) 
+
+
+#--- 
