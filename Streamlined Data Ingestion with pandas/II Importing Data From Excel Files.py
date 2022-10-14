9e4f9e5bfe93e57646db7b59bdd3daca37ad68b3
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-14 13:03:22 -0300

    Update II Importing Data From Excel Files.py

--- Streamlined Data Ingestion with pandas/II Importing Data From Excel Files.py
+++ Streamlined Data Ingestion with pandas/II Importing Data From Excel Files.py
@@ -1,1 +1,7 @@
-
+"""***************************************************************************************
+[] Spreadsheets:
+         - Unlike flatfiles, Can have Formatting and Formulas
+         - MUltiple spreadsheets can exist in a workbook
+         $$$ if is important info, export without formatting cause Pandas does not import spreadsheet formatting 
+   
+***************************************************************************************"""
