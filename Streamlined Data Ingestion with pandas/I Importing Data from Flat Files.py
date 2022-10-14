8da969b4f3f892c64e3c5eb9bda379344894ef59
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-14 11:23:27 -0300

    Update I Importing Data from Flat Files.py

--- Streamlined Data Ingestion with pandas/I Importing Data from Flat Files.py
+++ Streamlined Data Ingestion with pandas/I Importing Data from Flat Files.py
@@ -3,13 +3,15 @@
          - no format, 
          - separated by delimiter(,)
          - load them all >>>>>>>> read_csv()
-Loading CSV : 
+Loading CSV :
+===========
                   import pandas as pd
 
                   tax_data = read_csv("us_tax_data_2016.csv")
                   tax_data.head(4)
                   
 Loading other file types:
+=========================
           - specify a delmiter >>>>>> sep = ""
 ****************************************************************************************************"""
 
@@ -25,7 +27,7 @@ data = pd.read_csv("vt_tax_data_2016.csv")
 print(data.head())
 #```````````````````````````````````````````````````````````````````````````````````````````````
 
-#--- Get data from other flat files - \"t" - plot in bars
+#--- Get data from other flat files - sep=\"t" - plot in bars
 
 # Import pandas with the alias pd
 import pandas as pd
@@ -39,5 +41,30 @@ counts = data.groupby("agi_stub").N1.sum() # N1 contains income range categories
 counts.plot.bar()
 plt.show()
 #```````````````````````````````````````````````````````````````````````````````````````````````
-
+"""****************************************************************************************************
+Modifying flat file imports:
+===========================
+             - Limit columns >>>>>>>>> usecols
+                           col_names = ['STATEFIPS', 'STATE', 'zipcode', 'agi_stub', 'N1']
+                           col_nums = [0, 1, 2, 3, 4]
+                           
+                           # choose columns to load by name:
+                           tax_data_v1 = pd.read_csv("us_tax_data_2016.csv", usecols=col_names)
+             - Limit rows >>>>>>>>> nrows
+                           tax_data_first100 = pd.read_csv("us_tax_data_2016.csv", nrows=100)
+                           >>>>>>>>> skiprows -----accepts a list of row n, funtion to filter rows 
+                           >>>>>>>>> header=None ----------pandas knows there are no column names
+                           tax_data_next500 = pd.read_csv("us_tax_data_2016.csv",
+                                                            nrows=500,
+                                                            skiprows=1000,
+                                                            header= None)
+            - Asigning column Names >>>>>>>>>>> names
+                 $$$ do it after the import
+                           col_names = list(taxt_data_first1000)
+                           tax_data_next500 = pd.read_csv("us_tax_data_2016.csv",
+                                                            nrows=500,
+                                                            skiprows=1000,
+                                                            header= None,
+                                                            names= col_names)                                
+****************************************************************************************************"""
 #--- 
