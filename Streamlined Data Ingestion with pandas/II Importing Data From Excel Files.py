f01184ecf967c3cf4625135653aa591195d8bee1
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-14 13:36:20 -0300

    Update II Importing Data From Excel Files.py

--- Streamlined Data Ingestion with pandas/II Importing Data From Excel Files.py
+++ Streamlined Data Ingestion with pandas/II Importing Data From Excel Files.py
@@ -24,3 +24,26 @@ Modifiying imports:
                                         skiprows=2,
                                         usecols= "W:AB, AR")
 ***************************************************************************************"""
+
+#--- Get data from a spreadsheet
+# Load pandas as pd
+import pandas as pd
+
+# Read spreadsheet and assign it to survey_responses
+survey_responses = pd.read_excel("fcc_survey.xlsx")
+
+# View the head of the dataframe
+print(survey_responses.head())
+#```````````````````````````````````````````````````````````````````````````````````````````````
+
+#--- Load a portion of a spreadsheet
+# Create string of lettered columns to load
+col_string = "AD, AW:BA"
+
+# Load data with skiprows and usecols set
+survey_responses = pd.read_excel("fcc_survey_headers.xlsx", 
+                        skiprows=2, 
+                        usecols= col_string)
+
+# View the names of the columns selected
+print(survey_responses.columns)
