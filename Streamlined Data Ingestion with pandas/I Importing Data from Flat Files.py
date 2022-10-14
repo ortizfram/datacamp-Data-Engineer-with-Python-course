16142a0c2acdba5b3218eeaa33359535c8b6bcec
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-14 11:46:58 -0300

    Update I Importing Data from Flat Files.py

--- Streamlined Data Ingestion with pandas/I Importing Data from Flat Files.py
+++ Streamlined Data Ingestion with pandas/I Importing Data from Flat Files.py
@@ -67,4 +67,15 @@ Modifying flat file imports:
                                                             header= None,
                                                             names= col_names)                                
 ****************************************************************************************************"""
-#--- 
+
+#--- Import a subset of columns
+"""columns to use: zipcode, agi_stub (income group), mars1 (number of single households), MARS2 (number of households filing as married), and NUMDEP (number of dependents)."""
+
+# Create list of columns to use
+cols = list("zipcode","agi_stub","mars1","MARS2","NUMDEP")
+
+# Create dataframe from csv using only selected columns
+data = pd.DataFrame("vt_tax_data_2016.csv", usecols=cols)
+
+# View counts of dependents and tax returns by income level
+print(data.groupby("agi_stub").sum())
