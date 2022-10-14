39405f99f28595f99b09a40e574d9a9ea7ac127f
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-14 12:44:59 -0300

    Update I Importing Data from Flat Files.py

--- Streamlined Data Ingestion with pandas/I Importing Data from Flat Files.py
+++ Streamlined Data Ingestion with pandas/I Importing Data from Flat Files.py
@@ -137,4 +137,15 @@ data = pd.read_csv("vt_tax_data_2016.csv", dtype= data_types)
 
 # Print data types of resulting frame
 print(data.dtypes.head())
+#```````````````````````````````````````````````````````````````````````````````````````````````
                   
+#--- Set custom NA values
+# Create dict specifying that 0s in zipcode are NA values
+null_values = {"zipcode":0}
+
+# Load csv using na_values keyword argument
+data = pd.read_csv("vt_tax_data_2016.csv", 
+                   na_values= null_values)
+
+# View rows with NA ZIP codes
+print(data[data.zipcode.isna()])
