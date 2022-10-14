85ca870fb68aae133af57d11f49f6361a0dfd111
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-14 13:12:03 -0300

    Update II Importing Data From Excel Files.py

--- Streamlined Data Ingestion with pandas/II Importing Data From Excel Files.py
+++ Streamlined Data Ingestion with pandas/II Importing Data From Excel Files.py
@@ -2,6 +2,13 @@
 [] Spreadsheets:
          - Unlike flatfiles, Can have Formatting and Formulas
          - MUltiple spreadsheets can exist in a workbook
+         - load them all >>>>>>>> read_excel()
          $$$ if is important info, export without formatting cause Pandas does not import spreadsheet formatting 
-   
+   Loading Spreadsheets XLSX:
+=========================
+          import pandas as pd
+          # Read excel file
+          survey_data = pd.read_excel("fcc_survey.xlsl")
+          #view first 5 lines
+          print(survey_data.head())
 ***************************************************************************************"""
