241371f6aeb6b8f9be0dc9471f8f98d1dcc817b2
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-17 10:24:20 -0300

    Update II Importing Data From Excel Files.py

--- Streamlined Data Ingestion with pandas/II Importing Data From Excel Files.py
+++ Streamlined Data Ingestion with pandas/II Importing Data From Excel Files.py
@@ -92,7 +92,7 @@ plt.show()
 #---Select multiple sheets === sheet_name a list of names
 # Load both the 2016 and 2017 sheets by name
 all_survey_data = pd.read_excel("fcc_survey.xlsx",
-                                sheet_name=["2016",""])
+                                sheet_name=['2016','2017'])
 
 # View the data type of all_survey_data
 print(type(all_survey_data))
