dd404c8c275c3fbff97a5506dc165c10f3a93e76
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-17 11:39:23 -0300

    Update II Importing Data From Excel Files.py

--- Streamlined Data Ingestion with pandas/II Importing Data From Excel Files.py
+++ Streamlined Data Ingestion with pandas/II Importing Data From Excel Files.py
@@ -112,3 +112,20 @@ all_survey_data = pd.read_excel("fcc_survey.xlsx",
 
 # View the sheet names in all_survey_data
 print(all_survey_data.keys())
+#```````````````````````````````````````````````````````````````````````````````````````````````
+
+#---Work with multiple spreadsheets === compile similar spreadsheets into one dataset.
+# Create an empty dataframe
+all_responses = pd.DataFrame()
+
+# Set up for loop to iterate through values in responses
+for df in responses.values():
+  # Print the number of rows being added
+  print("Adding {} rows".format(df.shape[0]))
+  # Append df to all_responses, assign result
+  all_responses = all_responses.append(df)
+
+# Graph employment statuses in sample
+counts = all_responses.groupby("EmploymentStatus").EmploymentStatus.count()
+counts.plot.barh()
+plt.show()
