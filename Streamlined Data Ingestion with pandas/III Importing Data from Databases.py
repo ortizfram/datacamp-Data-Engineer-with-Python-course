3d6b9dad4609d35e7b18ab080e58ca1c6fb59db6
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-18 10:39:04 -0300

    Update III Importing Data from Databases.py

--- Streamlined Data Ingestion with pandas/III Importing Data from Databases.py
+++ Streamlined Data Ingestion with pandas/III Importing Data from Databases.py
@@ -164,4 +164,19 @@ print(temperatures)
 2  12/03/2017    48    427"""
 #``````````````````````````````````````````````````````````````````````````````````````````````````````
 
-#---
+#--- Selecting rows == a graph of safety call per city 
+# Create query to get hpd311calls records about safety
+query = """
+SELECT *
+FROM hpd311calls
+WHERE complaint_type=='SAFETY';
+"""
+
+# Query the database and assign result to safety_calls
+safety_calls = pd.read_sql(query, engine)
+
+# Graph the number of safety calls by borough(city)
+call_counts = safety_calls.groupby('borough').unique_key.count()
+call_counts.plot.barh()
+plt.show()
+#``````````````````````````````````````````````````````````````````````````````````````````````````````
