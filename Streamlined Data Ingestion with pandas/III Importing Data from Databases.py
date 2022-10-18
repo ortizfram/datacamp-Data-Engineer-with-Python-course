ae255f50e5656db73879cbbf2fc3b0e28fd49c24
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-18 12:51:45 -0300

    Update III Importing Data from Databases.py

--- Streamlined Data Ingestion with pandas/III Importing Data from Databases.py
+++ Streamlined Data Ingestion with pandas/III Importing Data from Databases.py
@@ -284,3 +284,46 @@ calls_by_issue = pd.read_sql(query, engine)
 # Graph the number of calls for each housing issue
 calls_by_issue.plot.barh(x="complaint_type")
 plt.show()
+#``````````````````````````````````````````````````````````````````````````````````````````````````````
+
+#--- Working with aggregate functions 1
+# Create a query to get month and max tmax by month
+query = """
+SELECT month, 
+       MAX(tmax)
+  FROM weather 
+  GROUP BY month;"""
+
+# Get dataframe of monthly weather stats
+weather_by_month = pd.read_sql(query, engine)
+
+# View weather stats by month
+print(weather_by_month)
+"""output:
+     month  MAX(tmax)
+0  December         61
+1  February         78
+2   January         61
+3     March         62"""
+#``````````````````````````````````````````````````````````````````````````````````````````````````````
+#--- Working with aggregate functions 2
+# Create a query to get month, max tmax, and min tmin by month
+query = """
+SELECT month, 
+	      MAX(tmax), 
+        MIN(tmin)
+FROM weather 
+GROUP BY month;
+"""
+
+# Get dataframe of monthly weather stats
+weather_by_month = pd.read_sql(query, engine)
+
+# View weather stats by month
+print(weather_by_month)
+"""output:
+   month  MAX(tmax)  MIN(tmin)
+0  December         61          9
+1  February         78         16
+2   January         61          5
+3     March         62         27"""
