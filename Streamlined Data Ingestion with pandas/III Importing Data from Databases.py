9164fff7369ff5624cf99374c3a86f9c16e6394d
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-18 12:36:23 -0300

    Update III Importing Data from Databases.py

--- Streamlined Data Ingestion with pandas/III Importing Data from Databases.py
+++ Streamlined Data Ingestion with pandas/III Importing Data from Databases.py
@@ -267,3 +267,20 @@ print(issues_and_boros)
     1       MANHATTAN          PLUMBING
     2       MANHATTAN    HEAT/HOT WATER
     3        BROOKLYN    HEAT/HOT WATER"""
+#``````````````````````````````````````````````````````````````````````````````````````````````````````
+
+#--- Counting in groups == graph counting issues
+# Create query to get call counts by complaint_type
+query = """
+SELECT complaint_type, 
+     COUNT(*)
+  FROM hpd311calls
+  GROUP BY complaint_type;
+"""
+
+# Create dataframe of call counts by issue
+calls_by_issue = pd.read_sql(query, engine)
+
+# Graph the number of calls for each housing issue
+calls_by_issue.plot.barh(x="complaint_type")
+plt.show()
