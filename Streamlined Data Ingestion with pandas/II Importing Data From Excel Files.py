88e07d3e7f698dad258a00454aa1f9fc8f7c69e4
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-17 16:37:49 -0800

    Update II Importing Data From Excel Files.py

--- Streamlined Data Ingestion with pandas/II Importing Data From Excel Files.py
+++ Streamlined Data Ingestion with pandas/II Importing Data From Excel Files.py
@@ -255,3 +255,17 @@ Modifying imports: parsing dates
                                                                         format=format_string)
                                        
 ****************************************************************************************"""
+
+#---Parse simple dates
+# Load file, with Part1StartTime parsed as datetime data
+survey_data = pd.read_excel("fcc_survey.xlsx",
+                            parse_dates=['Part1StartTime'])
+
+# Print first few values of Part1StartTime
+print(survey_data.Part1StartTime.head())
+"""output:
+ 0   2016-03-29 21:23:13
+    1   2016-03-29 21:24:59
+    2   2016-03-29 21:25:37
+    3   2016-03-29 21:21:37
+    4   2016-03-29 21:26:22"""
