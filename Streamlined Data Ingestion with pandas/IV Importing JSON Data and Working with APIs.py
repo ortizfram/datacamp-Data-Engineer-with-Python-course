6e7dbbae3667db2b4bef70c3539cd6e98551aa39
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-18 14:00:01 -0800

    Create IV Importing JSON Data and Working with APIs.py

---
+++ Streamlined Data Ingestion with pandas/IV Importing JSON Data and Working with APIs.py
@@ -0,0 +1,6 @@
+"""****************************************************************************************************
+ Learn how to work with JSON data and web APIs by exploring a public dataset and getting cafe recommendations from Yelp.
+End by learning some techniques to combine datasets once they have been loaded into data frames.
+
+****************************************************************************************************"""
+
