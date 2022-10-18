89ead173622a3299c5ea28599495a45524d7150d
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-18 13:57:49 -0800

    Update III Importing Data from Databases.py

--- Streamlined Data Ingestion with pandas/III Importing Data from Databases.py
+++ Streamlined Data Ingestion with pandas/III Importing Data from Databases.py
@@ -417,6 +417,7 @@ leak_calls = pd.read_sql(query,engine)
 
 # View the dataframe
 print(leak_calls.head())
+#``````````````````````````````````````````````````````````````````````````````````````````````````````
 
 #---Joining and filtering 2 === filter WHERE 'WATER LEAK
 # Query to get water leak calls and daily precipitation
@@ -432,3 +433,50 @@ leak_calls = pd.read_sql(query, engine)
 
 # View the dataframe
 print(leak_calls.head())
+#``````````````````````````````````````````````````````````````````````````````````````````````````````
+
+#---Joining, filtering, and aggregating 1
+# Query to get heat/hot water call counts by created_date
+query = """
+SELECT hpd311calls.created_date, 
+       COUNT(*)
+  FROM hpd311calls 
+  WHERE hpd311calls.complaint_type == 'HEAT'
+  GROUP BY hpd311calls.created_date;"""
+
+# Query database and save results as df
+df = pd.read_sql(query, engine)
+
+# View first 5 records
+print(df.head())
+#``````````````````````````````````````````````````````````````````````````````````````````````````````
+
+#---Joining, filtering, and aggregating 2
+# Modify query to join tmax and tmin from weather by date
+"""--count number of calls, saying temp max and min per day.
+(There is only one record per date in weather, so we do not need SQL's MAX and MIN functions here.)"""
+
+query = """
+SELECT hpd311calls.created_date, 
+	   COUNT(*), 
+       weather.tmax,
+       weather.tmin
+  FROM hpd311calls 
+       JOIN weather
+       ON hpd311calls.created_date = weather.date
+ WHERE hpd311calls.complaint_type = 'HEAT/HOT WATER' 
+ GROUP BY hpd311calls.created_date;
+ """
+
+# Query database and save results as df
+df = pd.read_sql(query, engine)
+
+# View first 5 records
+print(df.head())
+"""output:
+ created_date  COUNT(*)  tmax  tmin
+0   01/01/2018      4597    19     7
+1   01/02/2018      4362    26    13
+2   01/03/2018      3045    30    16
+3   01/04/2018      3374    29    19
+4   01/05/2018      4333    19     9"""
