6d36c3a2aa157892f2ddf393322272cb382df5f1
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-18 09:35:02 -0300

    Update III Importing Data from Databases.py

--- Streamlined Data Ingestion with pandas/III Importing Data from Databases.py
+++ Streamlined Data Ingestion with pandas/III Importing Data from Databases.py
@@ -22,4 +22,31 @@ This chapter features introductory SQL topics like WHERE clauses, aggregate func
             
                   SQL Alchemy library
                   ====================
+                  ## db connection with engine
+                  create_engine()       >>>>>>>> makes engine to handle db connections ==== takes string URL of db to connect to 
+                  sqlite:///filename.db >>>>>>>> SQLite URL Format 
+                  ## Query db
+                  pd.read_sql(query, engine) >>>>>>>> to pull data/ load in data
+                  query->table name >>>>>>>> to load whole table
+                  
+                  SQL Syntax (Line ENds with ;)
+                  ==========
+                  ## Basic SQL Syntax
+                  SELECT [column_names] FROM [table_name];
+                  ## Get all data in table 
+                  SELECT * FROM [table_name];
+                  
+           Getting data from a database
+           ============================
+           # Load pandas  and alchemy's create_engine
+           import pandas as pd
+           from alchemy import create_engine
+           
+           # create engine to manage db connection
+           engine = create_engine("sqlite:///"data.db")
+           
+           # Load entire table 2 ways (just table_name, or SELECT)
+           weather = pd.read_sql("weather", engine)
+           weather = pd.read_sql("SELECT * FROM weather", engine)
+****************************************************************************************************"""
                   
