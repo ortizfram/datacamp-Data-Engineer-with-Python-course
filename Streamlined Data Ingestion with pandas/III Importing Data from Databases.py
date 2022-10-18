40cfd4e0f4e237847b245f07c9a378e30e44bd03
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-18 09:02:10 -0300

    Update III Importing Data from Databases.py

--- Streamlined Data Ingestion with pandas/III Importing Data from Databases.py
+++ Streamlined Data Ingestion with pandas/III Importing Data from Databases.py
@@ -1,1 +1,25 @@
+"""****************************************************************************************************
+This chapter features introductory SQL topics like WHERE clauses, aggregate functions, and basic joins.
 
+  @@@ RELATIONAL databases
+      ====================
+            - Organized into tables
+            - Rows or records represents instance of an entity
+            - Columns of attributes
+            - Tables linked via Keys
+            - Support more data, multiple simultaneous users and data quality controls
+            - Data types specified/enforced for each column
+            - Interact with them via SQL 
+            
+            SQL Lite
+            ========
+            SQL Lite databases stored as regular, self-contained computer files. Great for sharing data
+  
+            Reading data/ Connecting to databases     
+            ====================================      
+            - Create a way to connect to the db
+            - Query
+            
+                  SQL Alchemy library
+                  ====================
+                  
