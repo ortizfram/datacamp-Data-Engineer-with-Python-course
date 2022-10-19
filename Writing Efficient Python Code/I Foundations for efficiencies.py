d6cfc7b5e2c4f44c29e53982c82d16b02f557078
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-19 12:01:50 -0800

    Update I Foundations for efficiencies.py

--- Writing Efficient Python Code/I Foundations for efficiencies.py
+++ Writing Efficient Python Code/I Foundations for efficiencies.py
@@ -1,1 +1,11 @@
-
+"""DEFINING EFFICIENT
+---------------------
+    - Fast runtime(- time)
+    - Minimal resource consumption(- memory)
+\\"""
+  # Non pythonic
+  doubled_numbers= []
+  for i in range(len(numbers)):
+     doubled_numbers.append(numbers[i]*2)
+  # Pythonic
+  doubled_numbers= []
