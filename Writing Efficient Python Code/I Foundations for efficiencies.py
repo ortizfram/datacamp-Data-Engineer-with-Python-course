62f6393db11a24c808d8a20e33e56d95f8a52d6c
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-20 14:23:05 -0800

    Update I Foundations for efficiencies.py

--- Writing Efficient Python Code/I Foundations for efficiencies.py
+++ Writing Efficient Python Code/I Foundations for efficiencies.py
@@ -165,3 +165,31 @@ $$ Alternative to Python Lists
                     nums_np[nums_np > 0]
                     #. [1,2]
 ******************************************************************************************************************"""
+## Practice with NumPy arrays 1
+
+# Print second row of nums
+print(nums[1,:])
+# [ 6  7  8  9 10]
+
+# Print all elements of nums that are greater than six
+print(nums[nums > 6])
+# [ 7  8  9 10]
+
+# Double every element of nums
+nums_dbl = nums * 2
+print(nums_dbl)
+# [[ 2  4  6  8 10]
+# [12 14 16 18 20]]
+    
+# Replace the third column of nums, adding 1 to each item in original column
+nums[:,2] = nums[:,2] + 1
+print(nums)
+# [[ 1  2  4  4  5]
+# [ 6  7  9  9 10]]
+#``````````````````````````````````````````````````````````````````````````````````````````````````
+"""Question
+
+When compared to a list object, what are two advantages of using a numpy array?
+        
+        #  A numpy array contains homogeneous data types (which reduces memory consumption) 
+            and provides the ability to apply operations on all elements through broadcasting."""
