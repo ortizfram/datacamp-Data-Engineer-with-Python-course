5e550fcec6d8abe535db2c0d680ee9d7319e5cc0
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-20 13:07:19 -0800

    Update I Foundations for efficiencies.py

--- Writing Efficient Python Code/I Foundations for efficiencies.py
+++ Writing Efficient Python Code/I Foundations for efficiencies.py
@@ -104,3 +104,19 @@ print(indexed_names_comp)
 indexed_names_unpack = [*enumerate(names, 1)] 
 print(indexed_names_unpack)
 """read as: list, unpack & enumerate(names, startingWithOne)"""
+#``````````````````````````````````````````````````````````````````````````````````````````````````
+## Built-in practice: map()
+
+# names = ['Jerry', 'Kramer', 'Elaine', 'George', 'Newman']
+
+# Use map to apply str.upper to each element in names
+names_map  = map(str.upper, names)
+
+# Print the type of the names_map
+print(type(names_map))
+
+# Unpack names_map into a list
+names_uppercase = [*names_map]
+
+# Print the list created above
+print(names_uppercase)
