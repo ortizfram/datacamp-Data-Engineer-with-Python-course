822080cadcb5c275d858a264f21bea4a8e6f3e51
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-21 17:59:19 -0300

    Create III Gaining efficiencies.py

---
+++ Writing Efficient Python Code/III Gaining efficiencies.py
@@ -0,0 +1,4 @@
+"""********************************************************************************************************************
+This chapter covers more complex efficiency tips and tricks. You'll learn a few useful built-in modules for writing efficient code and practice 
+using set theory. You'll then learn about looping patterns in Python and how to make them more efficient.
+********************************************************************************************************************"""
