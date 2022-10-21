c4f7b9fa2577584bae58fa02684021278c84dac6
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-21 08:55:42 -0300

    Update II Timing and profiling code.py

--- Writing Efficient Python Code/II Timing and profiling code.py
+++ Writing Efficient Python Code/II Timing and profiling code.py
@@ -1,1 +1,26 @@
+"""*******************************************************************************************************************************
+  You will learn how to gather and compare runtimes between different coding approaches. You'll practice using the line_profiler 
+and memory_profiler packages to profile your code base and spot bottlenecks. Then, you'll put your learnings to practice by replacing
+these bottlenecks with efficient Python code.
 
+    @ Why timing code?
+    
+      # Allows to pick 'optimal' code approach
+      # Faster == more 'efficient'
+      
+     How to time code?
+     =================
+    
+      >>>>>>> %timeit ====calculate runtime with IPython 'magic command'
+      >>>>>>> 'magic command' ===goes on 'top' of normal python syntax, prefixed by " % "
+      >>>>>>> %lsmagic ===== see all magic commands
+      
+          import numpy as np 
+          %timeit rand_nums = np.random.rand(1000)
+          # provides AVG of timing statistics
+          
+     Specify number of runs and loops
+     =================================
+      >>>>>>> -r ===set number of runs
+      >>>>>>> -n ===set number of loops
+*******************************************************************************************************************************"""
