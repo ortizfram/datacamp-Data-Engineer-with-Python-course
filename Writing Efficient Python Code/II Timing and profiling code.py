1b8aa6c27ba0ab34e7f036832f5368832a1cd2ce
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-21 09:19:05 -0300

    Update II Timing and profiling code.py

--- Writing Efficient Python Code/II Timing and profiling code.py
+++ Writing Efficient Python Code/II Timing and profiling code.py
@@ -23,4 +23,37 @@ these bottlenecks with efficient Python code.
      =================================
       >>>>>>> -r ===set number of runs
       >>>>>>> -n ===set number of loops
+      # loop : how many times to execute per run 
+      # run: how many runs 
+      
+          import numpy as np 
+          %timeit -r2 -n10 rand_nums = np.random.rand(1000)
+          
+     Timing lines, Timing Blocks
+     ===========================
+     >>>>>>> %timeit === time Line
+     >>>>>>> %%timeit === time Block
+     
+     Save Output into Variable (-o)
+     ===========================
+     times = %timeit -o rand_nums = np.random.rand(1000)
+     
+     ## to see ----- 'time for each run' 
+     times.timing
+     
+     ## to see ----- 'best time for all runs'
+     times.best
+     
+     ## to see ----- 'worst time for all runs'
+     times.worst
+     
+     Comparing times
+     ================
+     f_time = %timeit -o formal_dict = dict()
+     l_time = %timeit -o literal_dict = {}
+     
+     diff = (f_time.average - l_time.average) * (10**9)
+     print('l_time better than f_time by {} ns'.format(diff))
+     
+     # l_time better than f_time by 51.9 ns
 *******************************************************************************************************************************"""
