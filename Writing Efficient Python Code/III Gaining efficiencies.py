0e082ccde0caa6739e7bd60cf0e7373d4aa62fad
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-24 13:29:30 -0800

    Update III Gaining efficiencies.py

--- Writing Efficient Python Code/III Gaining efficiencies.py
+++ Writing Efficient Python Code/III Gaining efficiencies.py
@@ -423,3 +423,29 @@ print(*poke_zscores2[:3], sep='\n')
 """('Abomasnow', 80.0, 0.46797638117739043)
     ('Abra', 60.0, -0.3271693284337512)
     ('Absol', 131.0, 2.4955979406858013)"""
+#````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+## Bringing it all together: Pokémon z-scores 2
+
+# Calculate the total HP avg and total HP standard deviation
+hp_avg = hps.mean()
+hp_std = hps.std()
+
+# Use NumPy to eliminate the previous for loop
+z_scores = (hps - hp_avg)/hp_std
+
+# Combine names, hps, and z_scores
+poke_zscores2 = [*zip(names, hps, z_scores)]
+print(*poke_zscores2[:3], sep='\n')
+
+# Use list comprehension with the same logic as the highest_hp_pokemon code block
+highest_hp_pokemon2 = [(name, hp, zscore) for name,hp,zscore in poke_zscores2 if zscore > 2]
+print(*highest_hp_pokemon2, sep='\n')
+#````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+## Bringing it all together: Pokémon z-scores 3
+
+"""-----Question
+Use %%timeit (cell magic mode) within your IPython console to compare the runtimes between the original code
+blocks and the new code you developed using NumPy and list comprehension.
+Which approach was the faster?"""
+
+# The total time for executing the updated solution using NumPy and list comprehension was faster.
