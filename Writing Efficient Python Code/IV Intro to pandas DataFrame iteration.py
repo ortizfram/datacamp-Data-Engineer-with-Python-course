34751dc7805de9063f838c5a49f92418c8aa7907
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-24 13:36:19 -0800

    Create IV Intro to pandas DataFrame iteration.py

---
+++ Writing Efficient Python Code/IV Intro to pandas DataFrame iteration.py
@@ -0,0 +1,4 @@
+"""******************************************************************************************************************************************************
+   Brief introduction on how to efficiently work with pandas DataFrames.
+You'll learn the various options you have for iterating over a DataFrame. Then, you'll learn how to efficiently apply functions to data stored in a DataFrame.
+********************************************************************************************************************************************************"""
