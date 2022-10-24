5a0fb1d507d6df7b9dce46c8f5998ecb5479d0db
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-24 14:21:44 -0800

    Update IV Intro to pandas DataFrame iteration.py

--- Writing Efficient Python Code/IV Intro to pandas DataFrame iteration.py
+++ Writing Efficient Python Code/IV Intro to pandas DataFrame iteration.py
@@ -6,7 +6,31 @@ $$$$ PANDAS: library used for data analysis $$$$
 - DataFrame (tabular data structure)
 
 -----Best practices for iterating over a DataFrame
-   ^^ Calculating Win percentage
+
+  ++ Calculating Win percentage
+   
       import numpy as np
-      import pandas as pd
+      def calc_win_perc(wins, games_played):
+          win_perc = wins / games_played
+          return np.round(win_perc,2)
+      win_perc = calc_win_perc(50, 100)
+      print(win_perc)
+      # 0.5
+      
+  iterating with .iterrows()  (Pandas)    +efficient
+  ====================================
+  
+  >>>>>>>>> iterrrows() ==== returns index + each row as a tuple 
+  $$$$ .iterrrows() takes half the time that takes iloc() $$$$
+  
+  ++
+  
+     win_perc_list = []
+     
+     for i,row in baseball_df.iterrows():
+            wins = row['W']
+            games_played = row['G']
+            win_perc = calc_win_perc(wins,games_played)
+            win_perc_list.append(win_perc)
+     baseball_df['WP'] = win_perc_list
 ********************************************************************************************************************************************************"""
