ab6881f3fc4c59e7336cdb455aa4204709751157
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-24 13:51:44 -0800

    Update IV Intro to pandas DataFrame iteration.py

--- Writing Efficient Python Code/IV Intro to pandas DataFrame iteration.py
+++ Writing Efficient Python Code/IV Intro to pandas DataFrame iteration.py
@@ -1,4 +1,12 @@
 """******************************************************************************************************************************************************
    Brief introduction on how to efficiently work with pandas DataFrames.
 You'll learn the various options you have for iterating over a DataFrame. Then, you'll learn how to efficiently apply functions to data stored in a DataFrame.
+
+$$$$ PANDAS: library used for data analysis $$$$ 
+- DataFrame (tabular data structure)
+
+-----Best practices for iterating over a DataFrame
+   ^^ Calculating Win percentage
+      import numpy as np
+      import pandas as pd
 ********************************************************************************************************************************************************"""
