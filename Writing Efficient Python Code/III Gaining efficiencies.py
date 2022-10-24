1f3601cc84d2c0a6c71ea9ee30a006e46731c2f1
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-24 13:00:09 -0800

    Update III Gaining efficiencies.py

--- Writing Efficient Python Code/III Gaining efficiencies.py
+++ Writing Efficient Python Code/III Gaining efficiencies.py
@@ -387,4 +387,21 @@ generation 6: count =  68 percentage = 9.44"""
 """!!!
  You spotted a calculation that could be moved outside a loop to make the loop more efficient.
  When writing a loop is unavoidable, be sure to analyze the loop and move any one-time calculations outside."""
+#````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+## Holistic conversion loop
 
+# Collect all possible pairs using combinations()
+from itertools import combinations
+possible_pairs = [*combinations(pokemon_types, 2)]
+
+# Create an empty list called enumerated_tuples
+enumerated_tuples = []
+
+# Append each enumerated_pair_tuple to the empty list above
+for i,pair in enumerate(possible_pairs, 1):
+    enumerated_pair_tuple = (i,) + pair
+    enumerated_tuples.append(enumerated_tuples)
+
+# Convert all tuples in enumerated_tuples to a list
+enumerated_pairs = [*map(list, enumerated_tuples)]
+print(enumerated_pairs)
