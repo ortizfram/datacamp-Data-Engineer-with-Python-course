cd3935617de2610729f7bcfd15aa9b7059c32ca9
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-25 12:56:27 -0300

    Update I Best Practices.py

--- Writing Functions in Python/I Best Practices.py
+++ Writing Functions in Python/I Best Practices.py
@@ -117,3 +117,62 @@ DRY and "Do One Thing"
     / Use functions to avoid repetition
     - Refactor: improving code by changing it a little bit at a time
 **************************************************************************************************************************************************************"""
+## Extract a function
+
+def standardize(column):
+  """Standardize the values in a column.
+
+  Args:
+    column (pandas Series): The data to standardize.
+
+  Returns:
+    pandas Series: the values as z-scores
+  """
+  # Finish the function so that it returns the z-scores
+  z_score = (column - column.mean()) / column.std()
+  return z_score
+
+# Use the standardize() function to calculate the z-scores
+df['y1_z'] = standardize(df.y1_gpa)
+df['y2_z'] = standardize(df.y2_gpa)
+df['y3_z'] = standardize(df.y3_gpa)
+df['y4_z'] = standardize(df.y4_gpa)
+#`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+## Split up a function
+
+def mean(values):
+  """Get the mean of a sorted list of values
+  
+  Args:
+    values (iterable of float): A list of numbers
+
+  Returns:
+    float
+  """
+  # Write the mean() function
+  mean = sum(values) / len(values)
+  return mean
+
+## Split up a function 2
+
+def median(values):
+  """Get the median of a sorted list of values
+
+  Args:
+    values (iterable of float): A list of numbers
+
+  Returns:
+    float
+  """
+  # Write the median() function
+  midpoint = int(len(values) / 2)
+  if len(values) % 2 == 0:
+      median = (values[midpoint - 1] + values[midpoint]) / 2
+  else:
+      median = values[midpoint]
+  return  median
+"""**************************************************************************************************************************************************************
+ Pass by assignment
+===================
+
+ **************************************************************************************************************************************************************"""
