aa7652a88e39f850ef93b938baf9ebf4e17ac5b3
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-25 11:54:19 -0300

    Update I Best Practices.py

--- Writing Functions in Python/I Best Practices.py
+++ Writing Functions in Python/I Best Practices.py
@@ -32,3 +32,27 @@ Docstrings
             import inspect
             print(inspect.getdoc(func_name))
 **************************************************************************************************************************************************************"""
+## Crafting a docstring
+
+# Add a docstring to count_letter()
+def count_letter(content, letter):
+  """Count the number of times `letter` appears in `content`.
+  
+  # Add a Google style arguments section
+  Args:
+    content (str): The string to search.
+    letter (str): The letter to search for.
+    
+  # Add a returns section
+  Returns:
+    int
+    
+  # Add a section detailing what errors might be raised
+  Raises:
+    ValuError: If `letter` is not a one-character string
+  """
+  if (not isinstance(letter, str)) or len(letter) != 1:
+    raise ValueError('`letter` must be a single character string.')
+  return len([char for char in content if char == letter])
+
+
