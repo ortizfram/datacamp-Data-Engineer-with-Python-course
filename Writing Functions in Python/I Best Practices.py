a48c3553c6e0d5e31e350891c5e78706946e261e
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-25 12:03:25 -0300

    Update I Best Practices.py

--- Writing Functions in Python/I Best Practices.py
+++ Writing Functions in Python/I Best Practices.py
@@ -54,5 +54,47 @@ def count_letter(content, letter):
   if (not isinstance(letter, str)) or len(letter) != 1:
     raise ValueError('`letter` must be a single character string.')
   return len([char for char in content if char == letter])
+#`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+## Retrieving docstrings
 
+# Get the "count_letter" docstring by using an attribute of the function
+docstring = count_letter.__doc__
 
+border = '#' * 28
+print('{}\n{}\n{}'.format(border, docstring, border))
+
+## Retrieving docstrings 2
+
+import inspect
+
+# Inspect the count_letter() function to get its docstring
+docstring = inspect.getdoc(count_letter)
+
+"""add borders"""
+border = '#' * 28
+print('{}\n{}\n{}'.format(border, docstring, border))
+
+## Retrieving docstrings 3
+
+import inspect
+
+def build_tooltip(function):
+  """Create a tooltip for any function that shows the
+  function's docstring.
+
+  Args:
+    function (callable): The function we want a tooltip for.
+
+  Returns:
+    str
+  """
+  # Get the docstring for the "function" argument by using inspect
+  docstring = inspect.getdoc(function)
+  border = '#' * 28
+  return '{}\n{}\n{}'.format(border, docstring, border)
+
+print(build_tooltip(count_letter))
+print(build_tooltip(range))
+print(build_tooltip(print))
+"""!!!
+. But when we want to print the docstring, removing those leading spaces with inspect.getdoc() will look much better."""
