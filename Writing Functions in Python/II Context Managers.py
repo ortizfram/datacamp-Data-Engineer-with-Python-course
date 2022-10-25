cc5a6051e1ac2649d607ddf6050b4f469454246d
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-25 13:11:01 -0800

    Create II Context Managers.py

---
+++ Writing Functions in Python/II Context Managers.py
@@ -0,0 +1,5 @@
+"""********************************************************************************************************************************************
+If you've ever seen the "with" keyword in Python and wondered what its deal was, then this is the chapter for you! Context managers 
+are a convenient way to provide connections in Python and guarantee that those connections get cleaned up when you are done using them.
+This chapter will show you how to use context managers, as well as how to write your own.
+********************************************************************************************************************************************"""
