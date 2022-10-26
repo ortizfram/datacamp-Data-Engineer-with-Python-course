47e46dbd437dba220ccac1a3311d1f16fe990d7b
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-26 11:49:53 -0800

    Create I Manipulating files and directories.py

---
+++ Introduction to Shell/I Manipulating files and directories.py
@@ -0,0 +1,27 @@
+""" brief introduction to the Unix shell. You'll learn why it is still in use after almost 50 years, how it compares to the graphical tools
+you may be more familiar with, how to move around in the shell, and how to create, modify, and delete files and folders.
+"""
+
+
+## How does the shell compare to a desktop interface?
+"""---What is the relationship between the graphical file explorer that most people use and the command-line shell?"""
+# They are both interfaces for issuing commands to the operating system.
+
+
+## Where am I?
+  """ >>>>>> $ pwd === print working directory."""
+# /home/repl
+
+
+## How can I identify files and directories?
+  """ >>>>>> $ cd === go inside directory."""
+  """ >>>>>> $ ls ===  list the files in the directory."""
+
+"""---Which of these files is not in that directory?."""
+# $ cd seasonal
+# $ ls         
+# fall.csv
+
+
+## How else can I identify files and directories?
+""" **PATH : If it begins with /, it is absolute. If it does not begin with /, it is relative."""
