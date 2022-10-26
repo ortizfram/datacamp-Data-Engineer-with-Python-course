cb10394d8aba4f48bfb1edcfd2d68dc807445fea
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-26 11:00:05 -0800

    Update IV More on Decorators.py

--- Writing Functions in Python/IV More on Decorators.py
+++ Writing Functions in Python/IV More on Decorators.py
@@ -343,3 +343,75 @@ print(hello_goodbye('Alice'))
 
 """Timeout(): a real world example
 ================================="""
+
+## Tag your functions
+
+
+def tag(*tags):
+  # Define a new decorator, named "decorator", to return
+  def decorator(func):
+    # Ensure the decorated function keeps its metadata
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+      # Call the function being decorated and return the result
+      return func(*args, *kwargs)
+    wrapper.tags = tags
+    return wrapper
+  # Return the new decorator
+  return decorator
+
+@tag('test', 'this is a tag')
+def foo():
+  pass
+
+print(foo.tags)
+
+# ('test', 'this is a tag')
+#----
+
+## Check the return type
+
+
+def returns_dict(func):
+    # Complete the returns_dict() decorator
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        assert(type(result) == dict)
+        return result
+    return wrapper
+    
+@returns_dict
+def foo(value):
+  return value
+
+try:
+  print(foo([1,2,3]))
+except AssertionError:
+  print('foo() did not return a dict!')
+  
+# foo() did not return a dict!
+
+## Check the return type 2
+
+
+def returns(return_type):
+  # Complete the returns() decorator
+  def decorator(func):
+    def wrapper(*args, **kwargs):
+      result = func(*args, **kwargs)
+      assert type(result) == return_type
+      return result
+    return wrapper
+  return decorator
+  
+@returns(dict)
+def foo(value):
+  return value
+
+try:
+  print(foo([1,2,3]))
+except AssertionError:
+  print('foo() did not return a dict!')
+
+# <script.py> output:
+#    foo() did not return a dict!
