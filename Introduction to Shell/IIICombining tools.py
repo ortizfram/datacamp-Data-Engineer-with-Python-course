06de7aade0aae367e00be32e26517d5f5f1d57fb
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-27 12:10:13 -0300

    Update IIICombining tools.py

--- Introduction to Shell/IIICombining tools.py
+++ Introduction to Shell/IIICombining tools.py
@@ -1,3 +1,12 @@
 """
 This chapter will show you how to use this power to select the data you want, and introduce commands for sorting values and removing duplicates.
 """
+
+# How can I store a command's output in a file?
+"""Combine tail with redirection to save the last 5 lines of seasonal/winter.csv in a file called last.csv."""
+# $ tail -n 5 seasonal/winter.csv  > last.csv
+
+
+## How can I use a command's output as an input?
+"""Select the last two lines from seasonal/winter.csv and save them in a file called bottom.csv."""
+# $ tail -n 2 seasonal/winter.csv > bottom.csv
