146c35aee3a8493f1d1c30ad9a7d36ffde76b112
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-27 15:55:57 -0300

    Update III Combining tools.py

--- Introduction to Shell/III Combining tools.py
+++ Introduction to Shell/III Combining tools.py
@@ -57,3 +57,7 @@ Date,Tooth
       **   ? matches a single character, so 201?.txt will match 2017.txt or 2018.txt, but not 2017-01.txt.
       **   [...] matches any one of the characters inside the square brackets, so 201[78].txt matches 2017.txt or 2018.txt, but not 2016.txt.
       **   {...} matches any of the comma-separated patterns inside the curly brackets, so {*.txt, *.csv} matches any file whose name ends with .txt or .csv, but not files whose names end with .pdf.
+      
+--Which expression would match singh.pdf and johel.txt but not sandhu.pdf or sandhu.txt?"""
+ # {singh.pdf, j*.txt}
+
