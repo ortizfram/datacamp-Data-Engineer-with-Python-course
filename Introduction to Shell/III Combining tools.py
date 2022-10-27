a30fc5454229c7748cd0c08db4d4d7285793125b
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-27 15:39:11 -0300

    Update III Combining tools.py

--- Introduction to Shell/III Combining tools.py
+++ Introduction to Shell/III Combining tools.py
@@ -53,3 +53,7 @@ Date,Tooth
 
 
 ## What other wildcards can I use?
+""" ** wildcards less commonly used
+      **   ? matches a single character, so 201?.txt will match 2017.txt or 2018.txt, but not 2017-01.txt.
+      **   [...] matches any one of the characters inside the square brackets, so 201[78].txt matches 2017.txt or 2018.txt, but not 2016.txt.
+      **   {...} matches any of the comma-separated patterns inside the curly brackets, so {*.txt, *.csv} matches any file whose name ends with .txt or .csv, but not files whose names end with .pdf.
