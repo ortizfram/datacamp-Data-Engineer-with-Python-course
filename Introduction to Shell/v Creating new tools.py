6bb8b36dfb38ffa1ec326af273662642be63f189
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-28 13:40:49 -0300

    Create v Creating new tools.py

---
+++ Introduction to Shell/v Creating new tools.py
@@ -0,0 +1,4 @@
+"""
+History lets you repeat things with just a few keystrokes, and pipes let you combine existing commands to create new ones. In this chapter,
+you will see how to go one step further and create new commands of your own.
+"""
