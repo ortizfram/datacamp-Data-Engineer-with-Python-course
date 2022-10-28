77dd736e2bb973d08dbedcbc41076dbfbfcbaa65
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-28 15:06:14 -0300

    Update v Creating new tools.py

--- Introduction to Shell/v Creating new tools.py
+++ Introduction to Shell/v Creating new tools.py
@@ -2,3 +2,20 @@
 History lets you repeat things with just a few keystrokes, and pipes let you combine existing commands to create new ones. In this chapter,
 you will see how to go one step further and create new commands of your own.
 """
+
+## How can I edit a file?
+"""text editor Nano. If you type 
+      >>>>>> nano filename === it will open/create filename for editing
+          ** operations with control-key combinations:
+              Ctrl + K: delete a line.
+              Ctrl + U: un-delete a line.
+              Ctrl + O: save the file ('O' stands for 'output'). You will also need to press Enter to confirm the filename!
+              Ctrl + X: exit the editor."""
+"""---Run nano names.txt to edit a new file in your home directory and enter the following four lines:"""
+# $ nano names.txt
+# paste| Lovelace
+#        Hopper
+#        Johnson
+#        Wilson
+# cntrl + O : save  then press ENTER
+# cntrl + X : leave file
