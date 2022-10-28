c2b42e66ddd08d00ad5056b37d5df4e05bfeff54
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-28 11:12:56 -0300

    Update IV Batch processing.py

--- Introduction to Shell/IV Batch processing.py
+++ Introduction to Shell/IV Batch processing.py
@@ -10,3 +10,5 @@ see how the shell uses variables to store information.
          SHELL	Which shell program is being used	/bin/bash
          USER	User's ID	repl
          set   To get a complete list"""
+"""Use set and grep with a pipe to display the value of HISTFILESIZE, which determines how many old commands are stored in your command history. What is its value?"""
+# set | grep HISTFILESIZE
