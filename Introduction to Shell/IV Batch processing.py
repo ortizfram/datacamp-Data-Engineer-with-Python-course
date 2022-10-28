c0a07252e39675d13874c355faacdaadb1129088
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-28 11:07:31 -0300

    Create IV Batch processing.py

---
+++ Introduction to Shell/IV Batch processing.py
@@ -0,0 +1,12 @@
+"""
+hell commands will process many files at once. This chapter shows you how to make your own pipelines do that. Along the way, you will
+see how the shell uses variables to store information.
+"""
+
+## How does the shell store information?
+""" ** environment variables === 
+         HOME	User's home directory	/home/repl
+         PWD	Present working directory	Same as pwd command
+         SHELL	Which shell program is being used	/bin/bash
+         USER	User's ID	repl
+         set   To get a complete list"""
