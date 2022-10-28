88ed95f03e5d576f104583a367f65095e98ed973
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-28 10:35:05 -0300

    Update III Combining tools.py

--- Introduction to Shell/III Combining tools.py
+++ Introduction to Shell/III Combining tools.py
@@ -62,3 +62,50 @@ Date,Tooth
 singh.pdf and johel.txt but not sandhu.pdf or sandhu.txt?"""
  # {singh.pdf, j*.txt}
 
+
+## How can I sort lines of text?
+"""    >>>>>> sort ===  puts data in order...default it does this in ascending alphabetical order
+       >>>>>> flags --- -n(sort numerically), -r(reverse), -b(ignore leading blanks), -f(fold case)
+       ** Pipelines often use 'grep' to get rid of unwanted records and then 'sort' to put the remaining records in order."""
+"""eg.    cut -d , -f 2 seasonal/summer.csv | grep -v Tooth
+sort the names of the teeth in seasonal/winter.csv (not summer.csv) in descending alphabetical order. To do this, extend the pipeline with a sort step."""
+# $ cut -d , -f2 seasonal/winter.csv | grep -v Tooth | sort -r
+    
+   
+## How can I remove duplicate lines?
+"""   >>>>>> sort uniq === eliminate duplciates."""
+""" The start of your pipeline : cut -d , -f 2 seasonal/winter.csv | grep -v Tooth
+Extend it with a sort command, and use uniq -c to display unique lines with a count of how often each occurs """
+# $ cut -d , -f 2 seasonal/winter.csv | grep -v Tooth | sort | uniq -c
+
+
+## How can I save the output of a pipe?
+""" >>>>>>  > teeth-only.txt === save output in teeth-only
+      **  must appear at the end of the pipeline"""
+"""---What happens if we put redirection at the front of a pipeline as in:
+            > result.txt head -n 3 seasonal/winter.csv"""
+# The command's output is redirected to the file as usual.
+   
+   
+## How can I stop a running program?
+"""   >>>>>> Ctrl + C  or  ^C === to end it."""
+
+
+## Wrapping up
+"""build a pipeline to find out how many records are in the shortest of the seasonal data files."""
+"""Use wc with appropriate parameters to list the number of lines in all of the seasonal data files. (Use a wildcard for the filenames instead of typing them all in by hand.)"""
+# $ wc -l seasonal/*
+# out | 21 seasonal/autumn.csv
+  #24 seasonal/spring.csv
+  #25 seasonal/summer.csv
+  #26 seasonal/winter.csv
+  #96 total
+"""Add another command to the previous one using a pipe to remove the line containing the word "total"."""
+# $ wc -l seasonal/* | grep -v total
+# out | 21 seasonal/autumn.csv
+  #24 seasonal/spring.csv
+  #25 seasonal/summer.csv
+  #26 seasonal/winter.csv
+"""Add two more stages to the pipeline that use sort -n and head -n 1 to find the file containing the fewest lines."""
+# $ wc -l seasonal/* | grep -v total | sort -n | head -n 1
+# out |  21 seasonal/autumn.csv
