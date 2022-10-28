2c11d8c6a9a792e460d99f1525c3dbb4d121cfe1
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-28 12:33:59 -0300

    Update IV Batch processing.py

--- Introduction to Shell/IV Batch processing.py
+++ Introduction to Shell/IV Batch processing.py
@@ -29,3 +29,23 @@ see how the shell uses variables to store information.
 # $ testing=seasonal/winter.csv
 """Use head -n 1 SOMETHING to get the first line from seasonal/winter.csv using the value of the variable testing instead of the name of the file."""
 # $ head -n 1 $testing
+# out | Date,Tooth
+
+
+## How can I repeat a command many times?
+"""               ** loops
+eg.:      for filetype in gif jpg png; do echo $filetype; done
+output:  gif
+         jpg
+         png               
+"""
+"""Modify the loop so that it prints: docx   ,odt,    pdf"""
+# $ for filetype in docx odt pdf; do echo $filetype; done
+
+
+## How can I repeat a command once for each file?
+"""Modify the wildcard expression to people/* so that the loop prints the names of the files in the people directory
+regardless of what suffix they do or don't have. Please use filename as the name of your loop variable.
+"""
+# $ for filename in people/*; do echo $filename; done
+
