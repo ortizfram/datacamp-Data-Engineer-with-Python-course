e93ff3def1928d33d193ea6e162019b3d41084ea
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-31 13:54:27 -0800

    Update II Data Cleaning and Munging on the Command Line.py

--- Data Processing in Shell/II Data Cleaning and Munging on the Command Line.py
+++ Data Processing in Shell/II Data Cleaning and Munging on the Command Line.py
@@ -2,7 +2,7 @@
  utilize the command line library csvkit to convert, preview, filter and manipulate files to prepare our data for further analyses.
 """
 
-## THEO=======================
+## THEO============================================
 ## Getting started with csvkit
 ## ---------------------------
 """   ** CSVKIT : suit of command-line tools
@@ -23,7 +23,7 @@
    eg :  in2csv SpotifyData.xlsx --sheet "worksheet1_Popularity" > SpotifyData.csv
    eg2 : csvlook Spotify_Popularity.csv
    eg3 : csvstat Spotify_Popularity   """
-
+#..................................................
 
 
 ## EXECISES===================
@@ -46,3 +46,37 @@
  # unzip SpotifyData.zip
 """ Check to confirm name and location of unzipped file """
  # ls
+""" Convert SpotifyData.xlsx to csv"""
+ # in2csv SpotifyData.xlsx > SpotifyData.csv
+""" Print a preview in console using a csvkit suite command """
+ # csvlook SpotifyData.csv 
+ 
+ 
+ 
+## File conversion and summary statistics with csvkit
+""" Check to confirm name and location of the Excel data file """
+ # ls
+""" Convert sheet "Worksheet1_Popularity" to CSV """
+ # in2csv SpotifyData.xlsx --sheet "Worksheet1_Popularity" > Spotify_Popularity.csv
+""" Print high level summary statistics for each column """
+ # csvstat Spotify_Popularity.csv 
+#-------
+""" Check to confirm name and location of the Excel data file """
+ # ls
+""" Convert sheet "Worksheet2_MusicAttributes" to CSV """
+ # in2csv SpotifyData.xlsx --sheet "Worksheet2_MusicAttributes" > Spotify_MusicAttributes.csv
+""" Check to confirm name and location of the Excel data file """
+ # ls
+""" Convert sheet "Worksheet2_MusicAttributes" to CSV """
+ # in2csv SpotifyData.xlsx --sheet "Worksheet2_MusicAttributes" > Spotify_MusicAttributes.csv
+""" Check to confirm name and location of the new CSV file """
+ # ls
+""" Print preview of Spotify_MusicAttributes """
+ # csvlook Spotify_MusicAttributes.csv
+ #..............................................................
+ 
+ 
+ ## THEO=======================
+ ## Filtering data using csvkit
+ ## ---------------------------
+ 
