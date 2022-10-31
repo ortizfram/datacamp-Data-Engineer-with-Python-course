fb1cdb47720eb3ecbcead02488a400d59b472bc4
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-31 11:47:11 -0300

    Update I Downloading Data on the Command Line.py

--- Data Processing in Shell/I Downloading Data on the Command Line.py
+++ Data Processing in Shell/I Downloading Data on the Command Line.py
@@ -42,4 +42,6 @@ how to download data files from web servers via the command line. In the process
 
 ## Downloading single file using curl
 # Use curl to download the file from the redirected URL
-# curl -0 https://assets.datacamp.com/production/repositories/4180/datasets/eb1d6a36fa3039e4e00064797e1a1600d267b135/201812SpotifyData.zip
+  # curl -L https://assets.datacamp.com/production/repositories/4180/datasets/eb1d6a36fa3039e4e00064797e1a1600d267b135/201812SpotifyData.zip
+# # Download and rename the file in the same step
+  # curl -o Spotify201812.zip -L https://assets.datacamp.com/production/repositories/4180/datasets/eb1d6a36fa3039e4e00064797e1a1600d267b135/201812SpotifyData.zip
