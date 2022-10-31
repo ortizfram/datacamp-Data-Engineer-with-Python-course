fb38789324a265b7ea72d3d3f1bb6d1a58fa0d11
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-31 12:05:30 -0800

    Update I Downloading Data on the Command Line.py

--- Data Processing in Shell/I Downloading Data on the Command Line.py
+++ Data Processing in Shell/I Downloading Data on the Command Line.py
@@ -99,4 +99,53 @@ how to download data files from web servers via the command line. In the process
 
   
   
-## THEO==========Advanced downloading using Wget
+## THEO==========
+## Advanced downloading using Wget
+## ------------------------------------
+"""- save a list od file locations in txt file"""
+"""- Download from URL locations command read
+      >>>>>> -i       """
+## Limit rate for larger files  (--limit-rate)
+## ------------------------------------
+"""   >>>>>> __limit-rate
+      eg:  wget --limit-rate={rate}k {file_lcoation}
+      eg2: wget --limit-rate=200k -i url_list.txt     """
+## Limit rate for smaller files (wait)
+##-------------------------------------
+"""   >>>>>> --wait
+      eg:    wget --wait={seconds} {file_location}
+      eg2:   wget --wait= 2.5 -i url_list.txt       """
+
+
+
+## EXERCICES============
+
+## Setting constraints for multiple file downloads
+"""---Setting constraints for multiple file downloads using wget? """
+# Store all URL locations in a text file (e.g. url_list.txt) and iteratively download using wget and option flag i
+# cause iterates through the files but does not set any constraints for downloads.
+
+
+
+## Creating wait time using Wget
+""" View url_list.txt to verify content """
+  # cat url_list.txt
+""" Create a mandatory 1 second pause between downloading all files in url_list.txt """
+  # wget --wait=1 -i url_list.txt
+""" Take a look at all files downloaded """
+  # ls
+  
+  
+  
+## Data downloading with Wget and curl
+""" Use curl, download and rename a single file from URL"""
+  # curl -o Spotify201812.zip -L https://assets.datacamp.com/production/repositories/4180/datasets/eb1d6a36fa3039e4e00064797e1a1600d267b135/201812SpotifyData.zip
+""" Unzip, delete, then re-name to Spotify201812.csv  """
+  # unzip Spotify201812.zip && rm Spotify201812.zip
+  # mv 201812SpotifyData.csv Spotify201812.csv
+""" View url_list.txt to verify content """
+  # cat url_list.txt
+"""  Use Wget, limit the download rate to 2500KB/s, download all files in url_list.txt  """
+  # wget --limit-rate=2500k -i url_list.txt
+""" Take a look at all files downloaded """
+  # ls
