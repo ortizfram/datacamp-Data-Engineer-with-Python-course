b34440dfe1fb6c8fa9808f187caa4f2fb44cf2ba
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-31 12:39:28 -0300

    Update I Downloading Data on the Command Line.py

--- Data Processing in Shell/I Downloading Data on the Command Line.py
+++ Data Processing in Shell/I Downloading Data on the Command Line.py
@@ -45,3 +45,34 @@ how to download data files from web servers via the command line. In the process
   # curl -L https://assets.datacamp.com/production/repositories/4180/datasets/eb1d6a36fa3039e4e00064797e1a1600d267b135/201812SpotifyData.zip
 # # Download and rename the file in the same step
   # curl -o Spotify201812.zip -L https://assets.datacamp.com/production/repositories/4180/datasets/eb1d6a36fa3039e4e00064797e1a1600d267b135/201812SpotifyData.zip
+
+## Downloading multiple files using curl
+# Download all 100 data files
+  # curl -L https://s3.amazonaws.com/assets.datacamp.com/production/repositories/4180/datasets/files/datafile[001-100].txt
+# Print all downloaded files to directory
+  # ls datafile*.txt
+
+  
+  
+## Downloading data using Wget  :
+  """ ** Wget : Derives from World Wide Web and Get
+                compatible w/ all OS
+                Used to download data from HTTPS and FTP
+            $$$ BETER THAN USING CURL TO DOWNLOAD MULTIPLE FILES $$$      """
+## Check Installation   : 
+  # throws Wget location   : >>>>>> which wget 
+  """ if not installed follow link steps :  https://www.gnu.org/software/wget/    """
+  # check manual when installed :  >>>>>> man Wget
+  
+  
+## Basic Syntax Wget :  >>>>>> wget [option flags] [URL]
+
+
+""" ** Option Flags unique to wget :
+        -b : go background after startup
+        -q : turn off wget output
+        -c : resume broken download     
+       -------- 
+            eg. :  wget -bqc [URL]
+       ------
+    ** PID : unique ID for data download process in case of cancellation    """
