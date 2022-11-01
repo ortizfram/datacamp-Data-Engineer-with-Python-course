4edd742ac08197fee96ab98d2e5e26601934a1f7
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-01 12:15:00 -0300

    Update III Database Operations on the Command Line.py

--- Data Processing in Shell/III Database Operations on the Command Line.py
+++ Data Processing in Shell/III Database Operations on the Command Line.py
@@ -24,3 +24,36 @@ Focus on database operations we can do on the command line, including table crea
 """---what SQL database connections are currently NOT supported for sql2csv and for the rest of the csvkit suite?
    ** Pull up the documentation using csvsql -h   """
 # MongoDB
+
+
+
+### Practice pulling data from database
+# Verify database name 
+ls
+# Pull the entire Spotify_Popularity table and print in log
+sql2csv --db "sqlite:///SpotifyDatabase.db" \
+        --query "SELECT * FROM Spotify_Popularity" \
+#---
+# Verify database name 
+ls
+# Query first 5 rows of Spotify_Popularity and print in log
+sql2csv --db "sqlite:///SpotifyDatabase.db" \
+        --query "SELECT * FROM Spotify_Popularity LIMIT 5" \
+        | csvlook       
+#---
+# Verify database name 
+ls
+
+# Save query to new file Spotify_Popularity_5Rows.csv
+sql2csv --db "sqlite:///SpotifyDatabase.db" \
+        --query "SELECT * FROM Spotify_Popularity LIMIT 5" \
+        > Spotify_Popularity_5Rows.csv
+# Verify newly created file
+ls
+# Print preview of newly created file
+csvlook Spotify_Popularity_5Rows.csv
+
+## THEO ===============================
+### Manipulating data using SQL syntax
+
+#....................................................
