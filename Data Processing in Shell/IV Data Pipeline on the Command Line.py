2e6e78871cc5f056307b58bd8f87c5c1ea0b19c3
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-02 09:36:59 -0300

    Update IV Data Pipeline on the Command Line.py

--- Data Processing in Shell/IV Data Pipeline on the Command Line.py
+++ Data Processing in Shell/IV Data Pipeline on the Command Line.py
@@ -189,3 +189,29 @@ Data job automation with cron
               **** month  (1-12)
               ***** day of week (1-7)
 """
+### Understanding cron scheduling syntax
+"""
+-Which of the following is the correct Crontab syntax for execute the Python model script (model.py) every hour, on the 15 minute of an hour?
+    (e.g. 12:15 PM, 1:15 AM, 2:15 AM, etc)?
+    
+Answer : 15 * * * * python model.py
+"""
+
+### Scheduling a job with crontab
+"""
+-Verify that there are currently no CRON jobs currently scheduled via CRONTAB.
+"""
+# Verify that there are no CRON jobs currently scheduled
+crontab -l 
+
+# Create Python file hello_world.py
+echo "print('hello world')" > hello_world.py
+
+# Preview Python file 
+cat hello_world.py
+
+# Add as job that runs every minute on the minute to crontab
+echo "* * * * * python hello_world.py" | crontab
+
+# Verify that the CRON job has been added
+crontab -l
