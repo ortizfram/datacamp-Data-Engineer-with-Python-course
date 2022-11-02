f9664725ee6ac89ca3b87d550abff768fc642112
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-02 09:53:50 -0300

    Create I From Command-Line to Bash Script.py

---
+++ Introduction to Bash Scripting/I From Command-Line to Bash Script.py
@@ -0,0 +1,10 @@
+"""~~~
+Save yourself time when performing complex repetitive tasks. You’ll begin this course by refreshing your knowledge of common command-line programs and arguments
+before quickly moving into the world of Bash scripting. You’ll create simple command-line pipelines and then transform these into Bash scripts. You’ll then boost 
+your skills and learn about standard streams and feeding arguments to your Bash scripts.
+"""
+
+"""
+Introduction and refresher
+
+"""
