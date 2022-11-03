bba218f966a4f3a1f7db7dfa871ad26acaa58b65
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-03 10:34:48 -0300

    Create III Control Statements in Bash Scripting.py

---
+++ Introduction to Bash Scripting/III Control Statements in Bash Scripting.py
@@ -0,0 +1,4 @@
+"""
+ You’ll learn the differences between FOR, WHILE, IF, and CASE statements and how to use them in your Bash scripts.
+ Armed with these new tools, you’ll be ready to create more advanced Bash scripts with conditional logic.
+ """
