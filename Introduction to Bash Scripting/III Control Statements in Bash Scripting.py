d012283812f0a40657bb7e9cd1c5536c87cb1c82
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-04 12:25:21 -0300

    Update III Control Statements in Bash Scripting.py

--- Introduction to Bash Scripting/III Control Statements in Bash Scripting.py
+++ Introduction to Bash Scripting/III Control Statements in Bash Scripting.py
@@ -273,10 +273,37 @@ It is a Weekend!
 """
 ### Moving model results with CASE
 
+Your task is to use a CASE statement to move the tree-based models (Random Forest, GBM, and XGBoost)
+to the tree_models/ folder, and delete all other models (KNN and Logistic).
+
 Instructions
 
 - Use a FOR statement to loop through (using glob expansion) files in model_out/.
 - Use a CASE statement to match on the contents of the file (we will use cat and shell-within-a-shell to get the contents to match against). It must check if the 
    text contains a tree-based model name and move to tree_models/, otherwise delete the file.
 - Create a default match that prints out Unknown model in FILE where FILE is the filename then run your script.
+"""
+# Use a FOR loop for each file in 'model_out/'
+for file in model_out/*
+do
+    # Create a CASE statement for each file's contents
+    case $(cat $file) in
+      # Match on tree and non-tree models
+      *"Random Forest"*|*GBM*|*XGBoost*)
+      mv $file tree_models/ ;;
+      *KNN*|*Logistic*)
+      rm $file ;;
+      # Create a default
+      *) 
+      echo "Unknown model in $file" ;;
+    esac
+done
+
+"""
+### Finishing a CASE statement
+
+-What is the correct way to finish a CASE statement?
+ Answer the question
+ 
+Answer : esac
 """
