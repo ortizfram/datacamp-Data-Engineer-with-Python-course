56a37273207274a2851b7fc791c187ead65c8dbb
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-07 12:32:28 -0900

    Update III Test Organization and Execution.py

--- Unit Testing for Data Science in Python/III Test Organization and Execution.py
+++ Unit Testing for Data Science in Python/III Test Organization and Execution.py
@@ -5,25 +5,77 @@ your own project. Complete this chapter and become a unit testing wizard!
 """
 """
 \*** How to organize a growing set of tests?
-  _\project structure/_
-  -------------------
-   |src/  # all app code lives here
-   |  |--data/  # package for data pre-processing
-   |  |---__init__.py
-   |  |---preprocessing_helpers.py   # contains functions(); like row_to_list()
-   |--features/               # package for feature generation  from preprocessed data
-   |  |--__init__.py
-   |  |--as_numpy.py           # get_data_as_np_array()
-   |--models/                 # package for linear regression model
-   |  |--__init__.py
-   |  |--train.py
-   |tests/              # Test suite: all tests live here
-      |--data/  # package for data pre-processing
-      |   |---__init__.py
-      |   |---preprocessing_helpers.py   # contains functions(); like row_to_list()
-      |--features/               # package for feature generation  from preprocessed data
-      |   |--__init__.py
-      |   |--as_numpy.py           # get_data_as_np_array()
-      |--models/                 # package for linear regression model
-          |--__init__.py
-          |--train.py
+  \project structure/
+  
+     |src/  # all app code lives here
+     |  |--data/  # package for data pre-processing
+     |  |---__init__.py
+     |  |---preprocessing_helpers.py   # contains functions(); like row_to_list()
+     |--features/               # package for feature generation  from preprocessed data
+     |  |--__init__.py
+     |  |--as_numpy.py           # get_data_as_np_array()
+     |--models/                 # package for linear regression model
+     |  |--__init__.py
+     |  |--train.py
+     |tests/              # Test suite: all tests live here
+        |--data/  # package for data pre-processing
+        |   |---__init__.py
+        |   |---preprocessing_helpers.py   # contains functions(); like row_to_list()
+        |--features/               # package for feature generation  from preprocessed data
+        |   |--__init__.py
+        |   |--as_numpy.py           # get_data_as_np_array()
+        |--models/                 # package for linear regression model
+            |--__init__.py
+            |--train.py
+          
+  \Python module---to test_module correspondence/
+  
+      # module must have it's test module
+      >>>>>> x_module.py------test_x_module.py (inside test data)
+
+      @ test class:
+        > is a container for single unit tests  
+        |eg: 
+             class TestRowToList(object):                 # Always put arg object  # Use CamelCase
+                  def test_on_no_tab_missing_value(self):   #Always put self arg
+                  ...
+                  def test_on_two_tab_missing_value(self):  #Always put self arg 
+"""
+#|
+### Place test modules at the correct location
+"""In the package, there is a Python module plots.py, which contain functions related to plotting. These functions should be tested in a test module test_plots.py.
+-According to pytest guidelines, where should you place this test module within the project structure?"""
+tests/visualization/test_plots.py.
+#|
+#|
+### Create a test class
+""" Instructions
+- Declare the test class for the function split_into_training_and_testing_sets(), making sure to give it a name that follows the standard naming convention.
+- Fill in the mandatory argument in the test test_on_one_row()."""
+import pytest
+import numpy as np
+
+from models.train import split_into_training_and_testing_sets
+
+# Declare the test class
+class TestSplitIntoTrainingAndTestingSets(object):
+    # Fill in with the correct mandatory argument
+    def test_on_one_row(self):
+        test_argument = np.array([[1382.0, 390167.0]])
+        with pytest.raises(ValueError) as exc_info:
+            split_into_training_and_testing_sets(test_argument)
+        expected_error_msg = "Argument data_array must have at least 2 rows, it actually has just 1"
+        assert exc_info.match(expected_error_msg)
+"""
+  \Mastering test execution/
+      > runing all tests together
+      |eg: 
+          cd tests
+          pytest
+      # we might do this: after commit is pushed to code-base
+      > flag to stop after first failing test
+      >>>>>> pytest -x
+      > runing tests in a test module
+      >>>>>> pytest data/test_preprocessing_helpers.py
+.....      
+"""
