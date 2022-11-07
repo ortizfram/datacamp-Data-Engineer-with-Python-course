2e7d8ea0359e71ee3c9bebdda1b12ef731ef3419
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-07 12:15:10 -0300

    Create II Intermediate unit testing.py

---
+++ Unit Testing for Data Science in Python/II Intermediate unit testing.py
@@ -0,0 +1,6 @@
+"""
+testing complicated data types like NumPy arrays to testing exception handling, you'll do it all. Once you have mastered the science of testing,
+we will also focus on the arts. For example, we will learn how to find the balance between writing too many tests and too few tests. In the last lesson,
+you will get introduced to a radically new programming methodology called Test Driven Development (TDD) and put it to practice. This might actually change
+the way you code forever!
+"""
