7b5fdf6011fc9a58005a6c4c3b7aad1bfc0c0d52
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-07 13:54:56 -0300

    Update II Intermediate unit testing.py

--- Unit Testing for Data Science in Python/II Intermediate unit testing.py
+++ Unit Testing for Data Science in Python/II Intermediate unit testing.py
@@ -100,3 +100,28 @@ def test_on_clean_file():
   message = "Expected return value: {0}, Actual return value: {1}".format(expected, actual)
   # Complete the assert statement
   assert actual == pytest.approx(expected), message
+
+"""
+### Testing with multiple assert statements
+Instructions 1/4
+- Calculate the expected number of rows of the training array using the formula int(0.75*n), where n is the number of rows in example_argument,
+  and assign the variable expected_training_array_num_rows to this number.
+- Calculate the expected number of rows of the testing array using the formula n - int(0.75*n), where n is the number of rows in example_argument,
+  and assign the variable expected_testing_array_num_rows to this number.
+- Write an assert statement that checks if training array has expected_training_array_num_rows rows.
+- Write an assert statement that checks if testing array has expected_testing_array_num_rows rows.
+"""
+def test_on_six_rows():
+    example_argument = np.array([[2081.0, 314942.0], [1059.0, 186606.0],
+                                 [1148.0, 206186.0], [1506.0, 248419.0],
+                                 [1210.0, 214114.0], [1697.0, 277794.0]]
+                                )
+    # Fill in with training array's expected number of rows
+    expected_training_array_num_rows = 4
+    # Fill in with testing array's expected number of rows
+    expected_testing_array_num_rows = 2
+    actual = split_into_training_and_testing_sets(example_argument)
+    # Write the assert statement checking training array's number of rows
+    assert actual[0].shape[0] == expected_training_array_num_rows, "The actual number of rows in the training array is not {}".format(expected_training_array_num_rows)
+    # Write the assert statement checking testing array's number of rows
+    assert actual[1].shape[1] == expected_testing_array_num_rows, "The actual number of rows in the testing array is not {}".format(expected_testing_array_num_rows)
