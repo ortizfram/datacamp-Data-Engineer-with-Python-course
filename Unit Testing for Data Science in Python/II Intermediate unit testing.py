96d1254ff7a48719a72c6f955ffe8d8082c179bd
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-07 11:27:02 -0900

    Update II Intermediate unit testing.py

--- Unit Testing for Data Science in Python/II Intermediate unit testing.py
+++ Unit Testing for Data Science in Python/II Intermediate unit testing.py
@@ -299,3 +299,175 @@ def test_on_one_tab_with_missing_value():    # (1, 1) boundary value
     actual = row_to_list("\t4,567\n")
     # Format the failure message
     assert actual is None, "Expected: None, Actual: {0}".format(actual)                      
+
+"""
+### Testing well: Values triggering special logic
+Instructions
+- Assign the variable actual to the actual return value for "\n".
+- Complete the assert statement for test_on_no_tab_with_missing_value(), making sure to format the failure message appropriately.
+- Assign the variable actual to the actual return value for "123\t\t89\n".
+- Complete the assert statement for test_on_two_tabs_with_missing_value(), making sure to format the failure message appropriately.
+"""
+import pytest
+from preprocessing_helpers import row_to_list
+
+def test_on_no_tab_with_missing_value():    # (0, 1) case
+    # Assign to the actual return value for the argument "\n"
+    actual = row_to_list("\n")
+    # Write the assert statement with a failure message
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
+    
+def test_on_two_tabs_with_missing_value():    # (2, 1) case
+    # Assign to the actual return value for the argument "123\t\t89\n"
+    actual = row_to_list("123\t\t89\n")
+    # Write the assert statement with a failure message
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
+                      
+"""
+### Testing well: Normal arguments
+- How many normal arguments is it recommended to test?
+Answer 1: At least two or three.
+"""
+""" 2/4
+- Assign the variable expected to the expected return value for the normal argument "123\t4,567\n".
+"""
+import pytest
+from preprocessing_helpers import row_to_list
+
+def test_on_normal_argument_1():
+    actual = row_to_list("123\t4,567\n")
+    # Fill in with the expected return value for the argument "123\t4,567\n"
+    expected = row_to_list("123\t4,567\n")
+    assert actual == expected, "Expected: {0}, Actual: {1}".format(expected, actual)
+""" 3/4
+- Complete the correct assert statement for test_on_normal_argument_2(), making sure to format the failure message appropriately.
+"""
+import pytest
+from preprocessing_helpers import row_to_list
+
+def test_on_normal_argument_1():
+    actual = row_to_list("123\t4,567\n")
+    # Fill in with the expected return value for the argument "123\t4,567\n"
+    expected = ["123", "4,567"]
+    assert actual == expected, "Expected: {0}, Actual: {1}".format(expected, actual)
+    
+def test_on_normal_argument_2():
+    actual = row_to_list("1,059\t186,606\n")
+    expected = ["1,059", "186,606"]
+    # Write the assert statement along with a failure message
+    assert actual == expected, "Expected: {0}, Actual: {1}".format(expected, actual)
+""" 4/4
+The tests for boundary values, values triggering special behavior and normal arguments have been written to a test module test_row_to_list.py.
+-Run the tests in the IPython shell. Which bugs does the function have?
+Answer : The function does not have any bugs.
+"""
+                      
+"""
+*** TDD (test driven development)
+   *** Unit tests BEFORE IMPLEMENTATION
+        - Can't be deprioritized
+        - Requierements clearer, implementation easier
+"""
+                      
+"""
+### TDD: Tests for normal arguments
+Instructions
+- Complete the assert statement for test_with_no_comma() by inserting the correct boolean expression.
+- Complete the assert statement for test_with_one_comma() by inserting the correct boolean expression.
+- Complete the assert statement for test_with_two_commas() by inserting the correct boolean expression.
+"""
+def test_with_no_comma():
+    actual = convert_to_int("756")
+    # Complete the assert statement
+    assert actual == 756, "Expected: 756, Actual: {0}".format(actual)
+    
+def test_with_one_comma():
+    actual = convert_to_int("2,081")
+    # Complete the assert statement
+    assert actual == 2081, "Expected: 2081, Actual: {0}".format(actual)
+    
+def test_with_two_commas():
+    actual = convert_to_int("1,034,891")
+    # Complete the assert statement
+    assert actual == 1034891, "Expected: 1034891, Actual: {0}".format(actual)
+                      
+"""
+### TDD: Requirement collection
+Instructions 1/2
+- Give a name to the test by using the standard name prefix that pytest expects followed by on_string_with_missing_comma.
+- Assign actual to the actual return value for the argument "12,72,891".
+- Complete the assert statement.
+"""
+# Give a name to the test for an argument with missing comma
+def test_on_string_with_missing_comma():
+    actual = convert_to_int("178100,301")
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
+    
+def test_on_string_with_incorrectly_placed_comma():
+    # Assign to the actual return value for the argument "12,72,891"
+    actual = convert_to_int("12,72,891") 
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
+    
+def test_on_float_valued_string():
+    actual = convert_to_int("23,816.92")
+    # Complete the assert statement
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
+"""
+2/2
+-Run it in the IPython console and read the test result report. What happens?
+@ All tests are passing.
+@ The test test_on_string_with_two_commas() is failing because the convert_to_int("1,034,891") returns None instead of the correct integer 1034891.
+@ All tests are failing with a NameError since convert_to_int() has not been implemented yet.
+"""
+In [1]: !pytest test_convert_to_int.py
+# All tests are failing with a NameError since convert_to_int() has not been implemented yet
+                      
+"""
+### TDD: Implement the function
+Instructions 1/4
+- Complete the if statement that checks if the i-th element of comma_separated_parts has length greater than 3.
+"""
+def convert_to_int(integer_string_with_commas):
+    comma_separated_parts = integer_string_with_commas.split(",")
+    for i in range(len(comma_separated_parts)):
+        # Write an if statement for checking missing commas
+        if len(comma_separated_parts[i]) > 3:
+            return None
+"""
+2/4
+-Complete the if statement that checks if any entry other than the 0-th entry of comma_separated_parts has a length not equal to 3.
+"""
+def convert_to_int(integer_string_with_commas):
+    comma_separated_parts = integer_string_with_commas.split(",")
+    for i in range(len(comma_separated_parts)):
+        # Write an if statement for checking missing commas
+        if len(comma_separated_parts[i]) > 3:
+            return None
+        # Write the if statement for incorrectly placed commas
+        if i != 0 and len(comma_separated_parts[i]) != 3:
+            return None
+"""
+3/4
+- Fill in the except clause with a ValueError, which is raised when trying to convert float valued strings e.g. 23816.92 to an integer.
+"""
+def convert_to_int(integer_string_with_commas):
+    comma_separated_parts = integer_string_with_commas.split(",")
+    for i in range(len(comma_separated_parts)):
+        # Write an if statement for checking missing commas
+        if len(comma_separated_parts[i]) > 3:
+            return None
+        # Write the if statement for incorrectly placed commas
+        if i != 0 and len(comma_separated_parts[i]) != 3:
+            return None
+    integer_string_without_commas = "".join(comma_separated_parts)
+    try:
+        return int(integer_string_without_commas)
+    # Fill in with a ValueError, when trying to convert float valued string
+    except ValueError:
+        return None
+"""
+4/4
+- Run it the IPython console and read the test result report. Did you implement the function correctly, or are there any bugs?
+"""
+In [1]: !pytest test_convert_to_int.py
+# All tests are passing and the implementation does not have a bug.
