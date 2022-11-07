2a900f761a7588ee43dce9b0f98600884853966b
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-07 08:40:44 -0300

    Create I Unit testing basics.py

---
+++ Unit Testing for Data Science in Python/I Unit testing basics.py
@@ -0,0 +1,5 @@
+"""
+you will get introduced to the pytest package and use it to write simple unit tests. You'll run the tests,
+interpret the test result reports and fix bugs. Throughout the chapter, we will use examples exclusively from the data preprocessing module of a linear regression 
+project, making sure you learn unit testing in the context of data science.
+"""
