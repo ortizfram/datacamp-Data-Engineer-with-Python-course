ccb66164e3a746046b3b71f41a4cf1af8e2f82f5
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-07 12:28:27 -0300

    Update II Intermediate unit testing.py

--- Unit Testing for Data Science in Python/II Intermediate unit testing.py
+++ Unit Testing for Data Science in Python/II Intermediate unit testing.py
@@ -4,3 +4,27 @@ we will also focus on the arts. For example, we will learn how to find the balan
 you will get introduced to a radically new programming methodology called Test Driven Development (TDD) and put it to practice. This might actually change
 the way you code forever!
 """
+
+"""
+*** Mastering assert statements
+
+    *** optional message argument
+        >>>>> assert boolean_expression, message
+            eg: 
+                assert 1==2 , "One is not equal to two!"
+                
+                # this will not be printed since assert passed
+                assert 1==1 , "this will not be printed since assert passed"
+                
+    *** Adding message to unit test
+        import pytest
+        
+        def test_for_missing_area_with_message():
+              actual = row_to_list("\t293,410\n")
+              expected = None
+              message = ("row_to_list('\t293,410\n')"
+                         "returned {0} instead"
+                         "of {1}".format(actual,expected)
+                         )
+             assert actual is expected, message
+"""
