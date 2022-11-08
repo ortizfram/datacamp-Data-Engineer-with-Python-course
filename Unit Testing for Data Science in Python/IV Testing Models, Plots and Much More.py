c494c24fa145c781526ead39435facadde0e61e9
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-08 13:10:39 -0300

    Update IV Testing Models, Plots and Much More.py

--- Unit Testing for Data Science in Python/IV Testing Models, Plots and Much More.py
+++ Unit Testing for Data Science in Python/IV Testing Models, Plots and Much More.py
@@ -26,11 +26,17 @@ to test matplotlib plots. By the end of this chapter, you will be ready to test 
         ## (function) where setup and teardown are placed; outside the test
         >>>>>> @pytest.fixture
               eg:|
+                  import os
+                  
                   @pytest.fixture
                   def my_fixture(tmpdir):
                       # Do Setup here
+                      file_path="xxx"
                       yield data
                       # Do Teardown here  (happends when has finished executing)
+                      # remove in teardown
+                      
+                      os.remove(file_path)
 """
 #|
 #|
@@ -61,3 +67,18 @@ def test_on_clean_file(clean_data_file):
 -In the setup, assign the variable file_path to the correct string.
 -After the setup, yield the variable file_path so that the test can use it.
 -In the teardown, remove the file."""
+import os
+@pytest.fixture
+def empty_file():
+    # Assign the file path "empty.txt" to the variable
+    file_path = "empty.txt"
+    open(file_path, "w").close()
+    # Yield the variable file_path
+    yield file_path
+    # Remove the file in the teardown
+    os.remove(file_path)
+    
+def test_on_empty_file(self, empty_file):
+    expected = np.empty((0, 2))
+    actual = get_data_as_numpy_array(empty_file, 2)
+    assert actual == pytest.approx(expected), "Expected: {0}, Actual: {1}".format(expected, actual)
