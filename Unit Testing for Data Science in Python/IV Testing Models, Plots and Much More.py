d54a09feaa928c78c8b5d601239a972198e7d2e9
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-08 11:14:30 -0900

    Update IV Testing Models, Plots and Much More.py

--- Unit Testing for Data Science in Python/IV Testing Models, Plots and Much More.py
+++ Unit Testing for Data Science in Python/IV Testing Models, Plots and Much More.py
@@ -154,3 +154,28 @@ def test_on_raw_data(self, raw_and_clean_data_file, mocker):
     # Replace the dependency with the bug-free mock
     convert_to_int_mock = mocker.patch("data.preprocessing_helpers.convert_to_int",
                                     side_effect = convert_to_int_bug_free)
+#|
+"""Instructions 3/4
+-Use the correct attribute which returns the list of calls to the mock, and check if the mock was called with this sequence of
+arguments: "1,801", "201,411", "2,002", "333,209", "1990", "782,911", "1,285", "389129"."""
+# Add the correct argument to use the mocking fixture in this test
+def test_on_raw_data(self, raw_and_clean_data_file, mocker):
+    raw_path, clean_path = raw_and_clean_data_file
+    # Replace the dependency with the bug-free mock
+    convert_to_int_mock = mocker.patch("data.preprocessing_helpers.convert_to_int",
+                                       side_effect=convert_to_int_bug_free)
+    preprocess(raw_path, clean_path)
+    # Check if preprocess() called the dependency correctly
+    assert convert_to_int_mock.call_args_list == [call("1,801"), call("201,411"), call("2,002"), call("333,209"), call("1990"), call("782,911"), call("1,285"), call("389129")]
+    with open(clean_path, "r") as f:
+        lines = f.readlines()
+    first_line = lines[0]
+    assert first_line == "1801\\t201411\\n"
+    second_line = lines[1]
+    assert second_line == "2002\\t333209\\n" 
+#|
+"""instructions 4/4
+-Run the tests in TestPreprocess and TestConvertToInt. Based on the test result report, which of the following is correct? """
+!pytest -k "TestPreprocess"
+!pytest -k "TestConvertToInt"
+# Answ: Some tests for convert_to_int() fail but the test for preprocess() pass
