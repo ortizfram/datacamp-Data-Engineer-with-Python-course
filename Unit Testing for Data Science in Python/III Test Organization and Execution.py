2d62018d26ee7d072f3f7ca7b85e28fcd646e41c
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-08 10:29:20 -0300

    Update III Test Organization and Execution.py

--- Unit Testing for Data Science in Python/III Test Organization and Execution.py
+++ Unit Testing for Data Science in Python/III Test Organization and Execution.py
@@ -245,3 +245,51 @@ class TestModelTest(object):
         test_input = np.array([1.0, 2.0, 3.0, 4.0])
         with pytest.raises(ValueError) as exc_info:
             model_test(test_input, 1.0, 1.0)
+#|
+#|
+### Mark a test as conditionally skipped
+"""Instructions 1/4
+Question
+-Run the tests in the test class TestGetDataAsNumpyArray in the IPython console. What is the outcome?"""
+!pytest features/test_as_numpy.py::TestGetDataAsNumpyArray
+# Answ: The test test_on_clean_file() fails with a NameError because Python 3 does not recognize the xrange() function.
+#|
+"""2/4
+-Import the sys module."""
+import sys
+#|
+"""3/4
+-Mark the test test_on_clean_file() as skipped if the Python version is greater than 2.7."""
+# Import the sys module
+import sys
+
+class TestGetDataAsNumpyArray(object):
+    # Mark as skipped if Python version is greater than 2.7
+    @pytest.mark.skipif(sys.version_info > (2, 7))
+    def test_on_clean_file(self):
+        expected = np.array([[2081.0, 314942.0],
+                             [1059.0, 186606.0],
+                             [1148.0, 206186.0]
+                             ]
+                            )
+        actual = get_data_as_numpy_array("example_clean_data.txt", num_columns=2)
+        message = "Expected return value: {0}, Actual return value: {1}".format(expected, actual)
+        assert actual == pytest.approx(expected), message
+#|
+"""4/4
+-Add the following reason for skipping the test: "Works only on Python 2.7 or lower"."""
+# Import the sys module
+import sys
+
+class TestGetDataAsNumpyArray(object):
+    # Add a reason for skipping the test
+    @pytest.mark.skipif(sys.version_info > (2, 7), reason="Works only on Python 2.7 or lower")
+    def test_on_clean_file(self):
+        expected = np.array([[2081.0, 314942.0],
+                             [1059.0, 186606.0],
+                             [1148.0, 206186.0]
+                             ]
+                            )
+        actual = get_data_as_numpy_array("example_clean_data.txt", num_columns=2)
+        message = "Expected return value: {0}, Actual return value: {1}".format(expected, actual)
+        assert actual == pytest.approx(expected), message
