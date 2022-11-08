01889566fdb3f407748520661f6d316fd7a36f23
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-08 10:50:29 -0900

    Update IV Testing Models, Plots and Much More.py

--- Unit Testing for Data Science in Python/IV Testing Models, Plots and Much More.py
+++ Unit Testing for Data Science in Python/IV Testing Models, Plots and Much More.py
@@ -35,6 +35,8 @@ to test matplotlib plots. By the end of this chapter, you will be ready to test 
                       yield data
                       # Do Teardown here  (happends when has finished executing)
                       # remove in teardown
+                      # Use the appropriate method to create an empty file in the temporary directory
+                      file_path = tmpdir.join("empty.txt")
                       
                       os.remove(file_path)
 """
@@ -86,3 +88,47 @@ def test_on_empty_file(self, empty_file):
 """Instructions 2/2
 Question
 - run the test test_on_empty_file(). What is the outcome?"""
+!pytest -k "TestGetDataAsNumpyArray"
+# Answ: The test passes.
+#|
+#|
+### Fixture chaining using tmpdir
+""" instructions 1/2
+-Add the correct argument to the fixture empty_file() so that it chains with the built-in fixture tmpdir.
+-Use the appropriate method to create an empty file "empty.txt" inside the temporary directory created by tmpdir.
+"""
+import pytest
+
+@pytest.fixture
+# Add the correct argument so that this fixture can chain with the tmpdir fixture
+def empty_file(tmpdir):
+    # Use the appropriate method to create an empty file in the temporary directory
+    file_path = tmpdir.join("empty.txt")
+    open(file_path, "w").close()
+    yield file_path
+#|
+"""Instructions 2/2
+Question
+-In what order will the setup and teardown of empty_file() and tmpdir be executed?"""
+# Answ: setup of tmpdir -> setup of empty_file() -> teardown of empty_file() -> teardown of tmpdir.
+#|
+#|
+"""
+\*** Mocking
+     |      #-> replace bugged files w/ MagicMock() file only in testing
+     L> test a function independently from dependencies
+      (-1 install packages:  
+            - pip install pytest-mock
+            - pip install unittest.mock
+      (-2 (inside test function) 
+            - add mocker -as argument
+            - add var _mock asigning mocker.path("<dependency with module name>")
+            - add side_effect
+"""
+#|
+#|
+### Program a bug-free dependency
+"""Instructions
+-Define a function convert_to_int_bug_free() which takes one argument called comma_separated_integer_string.
+-Assign return_values to the dictionary holding the correct return values in the context of the raw data file used in the test.
+-Return the correct return value by looking up the dictionary return_values for the key comma_separated_integer_string."""
