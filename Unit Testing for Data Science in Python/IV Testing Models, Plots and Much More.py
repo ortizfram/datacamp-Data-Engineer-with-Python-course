27fce06bd6b436e46c0467bd5444f9858413840d
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-08 11:25:48 -0300

    Create IV Testing Models, Plots and Much More.py

---
+++ Unit Testing for Data Science in Python/IV Testing Models, Plots and Much More.py
@@ -0,0 +1,4 @@
+"""
+advanced unit testing skills like setup, teardown and mocking. You will also learn how to write sanity tests for your data science models and how 
+to test matplotlib plots. By the end of this chapter, you will be ready to test real world data science projects!
+"""
