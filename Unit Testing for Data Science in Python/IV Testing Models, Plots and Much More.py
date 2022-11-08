05e1373468331bf2e52e7aaaa9f05a889096fc10
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-08 12:58:31 -0300

    Update IV Testing Models, Plots and Much More.py

--- Unit Testing for Data Science in Python/IV Testing Models, Plots and Much More.py
+++ Unit Testing for Data Science in Python/IV Testing Models, Plots and Much More.py
@@ -39,3 +39,18 @@ to test matplotlib plots. By the end of this chapter, you will be ready to test 
 -Add the correct decorator that would turn clean_data_file() into a fixture.
 -Pass an argument to the test test_on_clean_file() so that it uses the fixture.
 -Pass the clean data file path yielded by the fixture as the first argument to the function get_data_as_numpy_array()."""
+# Add a decorator to make this function a fixture
+@pytest.fixture
+def clean_data_file():
+    file_path = "clean_data_file.txt"
+    with open(file_path, "w") as f:
+        f.write("201\t305671\n7892\t298140\n501\t738293\n")
+    yield file_path
+    os.remove(file_path)
+    
+# Pass the correct argument so that the test can use the fixture
+def test_on_clean_file(clean_data_file):
+    expected = np.array([[201.0, 305671.0], [7892.0, 298140.0], [501.0, 738293.0]])
+    # Pass the clean data file path yielded by the fixture as the first argument
+    actual = get_data_as_numpy_array(clean_data_file, 2)
+    assert actual == pytest.approx(expected), "Expected: {0}, Actual: {1}".format(expected, actual) 
