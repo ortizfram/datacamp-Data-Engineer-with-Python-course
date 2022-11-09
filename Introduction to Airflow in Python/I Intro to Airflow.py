df1dc12f8652544e05b16d934d740d82097ead0c
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-09 10:25:50 -0300

    Update I Intro to Airflow.py

--- Introduction to Airflow in Python/I Intro to Airflow.py
+++ Introduction to Airflow in Python/I Intro to Airflow.py
@@ -24,11 +24,13 @@ complete introduction to the components of Apache Airflow and learn how and why 
             dag_id = 'etl_pipeline',
             default_args = {"start_data":"2020-01-08"}
             )
+      ------
             
-    | Run a workflow in Airflow |
+    | Run a workflow in Airflow eg: |
     
     airflow run <dag_ig> <task_id> <start_date>
     airflow run example_etl download-file 2020-01-10
+    -------
 """
 #|
 #|
@@ -40,7 +42,7 @@ airflow run etl_pipeline download_file 2020-01-08
 ### Examining Airflow commands
 """-Which of the following is NOT an Airflow sub-command?
 
-    | get airflow documentation |
+    | get airflow documentation ariflow description |
     
     airflow -h
     
@@ -50,3 +52,75 @@ airflow run etl_pipeline download_file 2020-01-08
 -scheduler
 """
 # Answ: edit_dag
+#|
+#|
+"""
+\ What's a DAG /
+
+  > Direct : flow representing dependencies
+  > Acyclic : do not loop
+  > Graph : actual set of components
+  
+    # written in python, components in any language
+    # made up of components : tasks to be executed (operators, sensors)
+    # dependencies defined (explicit or implicit)
+    
+    | Define a DAG eg:|
+    
+    from airflow.models import DAG
+    from datetime import datetime
+    
+    default_arguments = {
+      'owner' : 'jdoe',
+      'emial' : 'jdoe@datacamp.com',
+      'start_date' : datetime(2020, 1, 20)
+    }
+    
+    etl_dag = DAG( 'etl_workflow', default_args=default_arguments )
+    ---------
+"""
+#|
+#|
+### Defining a simple DAG
+"""1/3"""
+# Import the DAG object
+from airflow.models import DAG
+#|
+"""2/3"""
+# Import the DAG object
+from airflow.models import DAG
+
+# Define the default_args dictionary
+default_args = {
+  'owner': 'dsmith',
+  'start_date': datetime(2020, 1, 14),
+  'retries' : 2
+}
+#|
+"""3/3"""
+# Import the DAG object
+from airflow.models import DAG
+
+# Define the default_args dictionary
+default_args = {
+  'owner': 'dsmith',
+  'start_date': datetime(2020, 1, 14),
+  'retries': 2
+}
+
+# Instantiate the DAG object
+etl_dag = DAG( 'example_etl', default_args= default_args)
+#|
+#|
+### Working with DAGs and the Airflow shell
+"""-How many DAGs are present in the Airflow system from the command-line?
+    
+    | list DAGs inshell |
+    
+    airflow list_dags
+"""
+airflow list_dags
+# Answ: 2
+#|
+#|
+### Troubleshooting DAG creation
