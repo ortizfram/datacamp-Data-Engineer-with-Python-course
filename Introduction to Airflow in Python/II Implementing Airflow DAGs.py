881644388947a2930cb40e3259dfd472b447368a
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-09 11:46:43 -0300

    Update II Implementing Airflow DAGs.py

--- Introduction to Airflow in Python/II Implementing Airflow DAGs.py
+++ Introduction to Airflow in Python/II Implementing Airflow DAGs.py
@@ -25,7 +25,7 @@ basics of implementing Airflow DAGs. Through hands-on activities, you’ll learn
   | Bash Operator egs: |
   
   #import Bash op
-  from airflow.operator.bash_operator import BashOperator
+  from airflow.operators.bash_operator import BashOperator
   
   example_task= BashOperator(
       task_id='bash_ex',
@@ -42,4 +42,28 @@ basics of implementing Airflow DAGs. Through hands-on activities, you’ll learn
 #|
 #|
 ### Defining a BashOperator task
+# Import the BashOperator
+from airflow.operators.bash_operator import BashOperator
 
+# Define the BashOperator 
+cleanup = BashOperator(
+    task_id='cleanup_task',
+    # Define the bash_command
+    bash_command='cleanup.sh',
+    # Add the task to the dag
+    dag=analytics_dag
+)
+#|
+#|
+### Multiple BashOperators
+# Define a second operator to run the `consolidate_data.sh` script
+consolidate = BashOperator(
+    task_id='consolidate_task',
+    bash_command='consolidate_data.sh',
+    dag=analytics_dag)
+
+# Define a final operator to execute the `push_data.sh` script
+push_data = BashOperator(
+    task_id='pushdata_task',
+    bash_command='push_data.sh',
+    dag=analytics_dag)
