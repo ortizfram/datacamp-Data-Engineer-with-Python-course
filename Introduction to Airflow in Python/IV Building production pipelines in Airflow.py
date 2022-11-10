7c0503e470ac8f3e9144755f2871236e5b1e26de
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-10 11:16:51 -0300

    Update IV Building production pipelines in Airflow.py

--- Introduction to Airflow in Python/IV Building production pipelines in Airflow.py
+++ Introduction to Airflow in Python/IV Building production pipelines in Airflow.py
@@ -22,4 +22,26 @@ Put it all together. In this final chapter, you’ll apply everything you've lea
 #|
 #|
 ### Creating a templated BashOperator
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+default_args = {
+  'start_date': datetime(2020, 4, 15),
+}
+
+cleandata_dag = DAG('cleandata',
+                    default_args=default_args,
+                    schedule_interval='@daily')
+
+# Create a templated command to execute
+# 'bash cleandata.sh datestring'
+templated_command='''
+bash cleandata.sh {{ ds_nodash }}
+'''
+
+# Modify clean_task to use the templated command
+clean_task = BashOperator(task_id='cleandata_task',
+                          bash_command=templated_command,
+                          dag=cleandata_dag)
 
