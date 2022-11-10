337ec5bdf2fbb806afa02032323ec453f6ed4e52
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-10 12:39:06 -0300

    Update IV Building production pipelines in Airflow.py

--- Introduction to Airflow in Python/IV Building production pipelines in Airflow.py
+++ Introduction to Airflow in Python/IV Building production pipelines in Airflow.py
@@ -119,3 +119,33 @@ clean_task2 << clean_task
         > {{ macros.uuid }}
         > {{ macros.ds_add('2020-04-15', 5) }}  : modify days from date,  RETURNS: 2020-04-20
 """
+#|
+#|
+### Using lists with templates
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+filelist = [f'file{x}.txt' for x in range(30)]
+
+default_args = {
+  'start_date': datetime(2020, 4, 15),
+}
+
+cleandata_dag = DAG('cleandata',
+                    default_args=default_args,
+                    schedule_interval='@daily')
+
+# Modify the template to handle multiple files in a 
+# single run.
+templated_command = """
+  <% for filename in params.filenames %>
+  bash cleandata.sh {{ ds_nodash }} {{ filename }};
+  <% endfor %>
+"""
+
+# Modify clean_task to use the templated command
+clean_task = BashOperator(task_id='cleandata_task',
+                          bash_command=templated_command,
+                          params={'filenames': filelist},
+                          dag=cleandata_dag)
