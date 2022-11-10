a48c0dfc5fda6252672d455c9569cc832fded332
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-10 11:40:15 -0300

    Update IV Building production pipelines in Airflow.py

--- Introduction to Airflow in Python/IV Building production pipelines in Airflow.py
+++ Introduction to Airflow in Python/IV Building production pipelines in Airflow.py
@@ -44,4 +44,44 @@ bash cleandata.sh {{ ds_nodash }}
 clean_task = BashOperator(task_id='cleandata_task',
                           bash_command=templated_command,
                           dag=cleandata_dag)
+#|
+#|
+### Templates with multiple arguments
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
 
+default_args = {
+  'start_date': datetime(2020, 4, 15),
+}
+
+cleandata_dag = DAG('cleandata',
+                    default_args=default_args,
+                    schedule_interval='@daily')
+
+# Modify the templated command to handle a
+# second argument called filename.
+templated_command = """
+  bash cleandata.sh {{ ds_nodash }} {{ params.filename }}
+"""
+
+# Modify clean_task to pass the new argument
+clean_task = BashOperator(task_id='cleandata_task',
+                          bash_command=templated_command,
+                          params={'filename': 'salesdata.txt'},
+                          dag=cleandata_dag)
+
+# Create a new BashOperator clean_task2
+clean_task2 = BashOperator(task_id='cleandata_task2',
+                           bash_command=templated_command,
+                           params={'filename':'supportdata.txt'},
+                           dag=cleandata_dag)
+                           
+# Set the operator dependencies
+clean_task2 << clean_task
+#|
+#|
+"""
+\ more templates /
+
+...
