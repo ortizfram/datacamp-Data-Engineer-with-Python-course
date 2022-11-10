12a4f9775c633509d036615d228fa3d3a808df11
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-10 09:24:52 -0300

    Update III Maintaining and monitoring Airflow workflows.py

--- Introduction to Airflow in Python/III Maintaining and monitoring Airflow workflows.py
+++ Introduction to Airflow in Python/III Maintaining and monitoring Airflow workflows.py
@@ -15,7 +15,7 @@ you’ll learn how to save yourself time using Airflow components such as sensor
   
     mode : 'how to check for the condition'
         mode='poke' : 'default, run repeatedly'
-        mode='rechedule' : 'give task slot and try again'
+        mode='reschedule' : 'give task slot and try again, or run other task while waiting'
     poke_interval   : 'wait between checks'
     timeout   : 'how long to wait before failing task'
     
@@ -93,3 +93,87 @@ you’ll learn how to save yourself time using Airflow components such as sensor
       - via first line of : airflow list_dags
         INFO - Using SequentialExecutor
 """
+#|
+#|
+### Determining the executor
+airflow list_dags
+# ANSW: This system can run one task at a time.
+#|
+#|
+### Executor implications
+"""instructions
+airflow list_dags).
+Look at the source for the DAG file and fix which entry is causing the problem."""
+"""Answer :
+1. Open execute_report_dag.py
+2. Modification line 15 to mode='reschedule'
+3. Run in terminal command airflow list_dags"""
+#  gave Airflow a chance to run another task while waiting for the salesdata_ready.csv file
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.sensors.file_sensor import FileSensor
+from datetime import datetime
+
+report_dag = DAG(
+    dag_id = 'execute_report',
+    schedule_interval = "0 0 * * *"
+)
+
+precheck = FileSensor(
+    task_id='check_for_datafile',
+    filepath='salesdata_ready.csv',
+    start_date=datetime(2020,2,20),
+    mode='reschedule' ,
+    dag=report_dag
+)
+
+generate_report_task = BashOperator(
+    task_id='generate_report',
+    bash_command='generate_report.sh',
+    start_date=datetime(2020,2,20),
+    dag=report_dag
+)
+
+precheck >> generate_report_task
+#|
+#|
+"""
+\ Debugging and troubleshooting in Airflow /
+
+  | typical ISSUES |
+  
+   - task won't run on schedule :
+      ## check if scheduler running
+       # to fix run ... in command-line
+       airflow scheduler
+      
+      ## one schedule_interval may not passed
+       # modify attribute to meet requirements
+       
+      ## not enough task free within executor
+       # change executor type
+       # add OS resourses
+       # add other OSs
+       # change DAG schedule
+       
+   - DAG won't load
+      ## DAG not in Web UI
+      ## DAG not in airflow list_dags
+       # verify DAG is in correct folder
+       # determine  DAGs folder :
+       head airflow/airflow.cfg
+       
+   - Syntax errors
+      ## DAG file won't appear
+      ## diffucult to find errors in a DAG
+        airflow list_dags
+        python3 dagfile.py
+"""
+#|
+#|
+### DAGs in the bag
+head airflow/airflow.cfg
+# ANSW: The `dags_folder` is set to `/home/repl/workspace/dags`.
+#|
+#|
+### Missing DAG
