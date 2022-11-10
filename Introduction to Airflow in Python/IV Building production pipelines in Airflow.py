50b7a9c620fe8873f437791e756f8075a9efea2c
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-10 13:52:38 -0300

    Update IV Building production pipelines in Airflow.py

--- Introduction to Airflow in Python/IV Building production pipelines in Airflow.py
+++ Introduction to Airflow in Python/IV Building production pipelines in Airflow.py
@@ -153,3 +153,32 @@ clean_task = BashOperator(task_id='cleandata_task',
 #|
 ### Understanding parameter options
 # ANSW: Using specific tasks allows better monitoring of task state and possible parallel execution.
+#|
+#|
+### Sending templated emails
+from airflow.models import DAG
+from airflow.operators.email_operator import EmailOperator
+from datetime import datetime
+
+# Create the string representing the html email content
+html_email_str = """
+Date: {{ ds }}
+Username: {{ params.username }}
+"""
+
+email_dag = DAG('template_email_test',
+                default_args={'start_date': datetime(2020, 4, 15)},
+                schedule_interval='@weekly')
+
+email_task = EmailOperator(task_id='email_task',
+                           to='testuser@datacamp.com',
+                           subject="{{ macros.uuid.uuid4() }}",
+                           html_content=html_email_str,
+                           params={'username': 'testemailuser'},
+                           dag=email_dag)
+#|
+#|
+"""
+\ branching /
+
+    ...
