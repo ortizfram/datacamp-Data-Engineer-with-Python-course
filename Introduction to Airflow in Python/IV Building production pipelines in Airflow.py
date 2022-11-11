9cc4a728695e18555d9e7a4d9ac9c1af1ce28566
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-11 10:52:27 -0300

    Update IV Building production pipelines in Airflow.py

--- Introduction to Airflow in Python/IV Building production pipelines in Airflow.py
+++ Introduction to Airflow in Python/IV Building production pipelines in Airflow.py
@@ -292,24 +292,24 @@ from datetime import timedelta, datetime
 
 # Update the default arguments and apply them to the DAG
 default_args = {
-  'start_date': datetime(2019,1,1),
-  'sla':timedelta(minutes=90)
+    'start_date': datetime(2019, 1, 1),
+    'sla': timedelta(minutes=90)
 }
 
 dag = DAG(dag_id='etl_update', default_args=default_args)
 
-sensor = FileSensor(task_id='sense_file', 
+sensor = FileSensor(task_id='sense_file',
                     filepath='/home/repl/workspace/startprocess.txt',
-                    poke_interval=45, # wait between checks
+                    poke_interval=45,
                     dag=dag)
 
-bash_task = BashOperator(task_id='cleanup_tempfiles', 
+bash_task = BashOperator(task_id='cleanup_tempfiles',
                          bash_command='rm -f /home/repl/*.tmp',
                          dag=dag)
 
-python_task = PythonOperator(task_id='run_processing', 
+python_task = PythonOperator(task_id='run_processing',
                              python_callable=process_data,
-                             provide_context= True,
+                             provide_context=True,
                              dag=dag)
 
 sensor >> bash_task >> python_task
