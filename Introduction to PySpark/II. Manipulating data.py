e1717f646c694ef9b4898d2a4994f5d4adf20b36
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-11 14:16:22 -0300

    Update II. Manipulating data.py

--- Introduction to PySpark/II. Manipulating data.py
+++ Introduction to PySpark/II. Manipulating data.py
@@ -39,3 +39,16 @@ flights = flights.withColumn('duration_hrs', flights.air_time /60)
 #|
 #|
 ### Filtering Data
+# Filter flights by passing a string
+long_flights1 = flights.filter("distance > 1000")   # sql string
+
+# Filter flights by passing a column of boolean values
+long_flights2 = flights.filter(flights.distance > 1000)
+
+# Print the data to check they're equal
+long_flights1.show()
+long_flights2.show()
+#- same result
+#|
+#|
+### Selecting
