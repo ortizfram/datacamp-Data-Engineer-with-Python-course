09cce932600f741ac826dab6d997c6fd4dd36aa2
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-14 11:43:02 -0300

    Update III. Getting started with machine learning pipelines.py

--- Introduction to PySpark/III. Getting started with machine learning pipelines.py
+++ Introduction to PySpark/III. Getting started with machine learning pipelines.py
@@ -96,3 +96,11 @@ carr_encoder = OneHotEncoder(imputCol='carrier_index',outputCol='carrier_fact')
 #|
 #|
 ### Destination
+# Create a StringIndexer
+dest_indexer = StringIndexer(inputCol='dest',outputCol='dest_index')
+
+# Create a OneHotEncoder
+dest_encoder = OneHotEncoder(inputCol='dest_index',outputCol='dest_fact')
+#|
+#|
+### Assemble a vector
