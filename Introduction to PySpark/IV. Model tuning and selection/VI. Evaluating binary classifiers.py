ff76de5dc91138e59cae3cb973b4e7bef517e3f8
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-14 12:45:36 -0900

    Create VI. Evaluating binary classifiers.py

---
+++ Introduction to PySpark/IV. Model tuning and selection/VI. Evaluating binary classifiers.py
@@ -0,0 +1,12 @@
+"""
+
+\  Evaluating binary classifiers  /
+
+For this course we'll be using a common metric for binary classification algorithms call the 
+
+  -> AUC (or area under the curve) :  the closer the AUC is to one (1), the better the model is!
+
+"""
+
+'''If you've created a perfect binary classification model, what would the AUC be?'''
+# ANSW: 1
