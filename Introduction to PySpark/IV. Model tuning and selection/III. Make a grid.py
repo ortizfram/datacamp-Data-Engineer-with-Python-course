fca1f29b02775e529b19bc11ee4c383270ef9794
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-14 10:56:37 -0900

    Update III. Make a grid.py

--- Introduction to PySpark/IV. Model tuning and selection/III. Make a grid.py
+++ Introduction to PySpark/IV. Model tuning and selection/III. Make a grid.py
@@ -1,3 +1,13 @@
+"""
+\  make a grid  /
+
+- Import the submodule pyspark.ml.tuning under the alias tune.
+- Call the class constructor ParamGridBuilder() with no arguments. Save this as grid.
+- Call the .addGrid() method on grid with lr.regParam as the first argument and np.arange(0, .1, .01) as the second argument. This second call is a function from the numpy module (imported as np) that creates a list of numbers from 0 to .1, incrementing by .01. Overwrite grid with the result.
+- Update grid again by calling the .addGrid() method a second time create a grid for lr.elasticNetParam that includes only the values [0, 1].
+- Call the .build() method on grid and overwrite it with the output.
+"""
+
 # Import the tuning submodule
 import pyspark.ml.tuning as tune
 
