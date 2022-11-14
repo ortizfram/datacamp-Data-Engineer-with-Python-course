f30615d550d8d03d5da73f851d74a3a5e6cba9b5
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-14 10:57:05 -0300

    Update III. Getting started with machine learning pipelines.py

--- Introduction to PySpark/III. Getting started with machine learning pipelines.py
+++ Introduction to PySpark/III. Getting started with machine learning pipelines.py
@@ -57,4 +57,12 @@ model_data = model_data.withColumn(
     "plane_age", model_data.year - model_data.plane_year)
 #|
 #|
-###
+### Making a Boolean
+# Create is_late bool
+model_data = model_data.withColumn("is_late", model_data.arr_delay >0)
+
+# Convert to an integer w/ .cast()
+model_data = model_data.withColumn("label", model_data.is_late.cast('integer'))
+
+# Remove missing values w/ SQL string
+model_data = model_data.filter("arr_delay is not NULL and dep_delay is not NULL and air_time is not NULL and plane_year is not NULL")
