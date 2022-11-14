3025e23010afa29ee1205324e43103b1f901b0f3
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-14 12:56:13 -0900

    Create I. Intro to AWS and Boto3.py

---
+++ Introduction to AWS Boto in Python/1. Putting Files in the Cloud!/I. Intro to AWS and Boto3.py
@@ -0,0 +1,6 @@
+"""
+|||||
+From learning how AWS works to creating S3 buckets and uploading files to them. You will master the basics of setting up AWS and uploading files to the cloud!
+|||||
+
+"""
