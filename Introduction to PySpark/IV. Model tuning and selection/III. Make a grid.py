6a36113599022b04d317c55864bd83b4e34d34a3
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-14 10:55:35 -0900

    Create III. Make a grid.py

---
+++ Introduction to PySpark/IV. Model tuning and selection/III. Make a grid.py
@@ -0,0 +1,12 @@
+# Import the tuning submodule
+import pyspark.ml.tuning as tune
+
+# Create the parameter grid
+grid = tune.ParamGridBuilder()
+
+# Add the hyperparameter
+grid = grid.addGrid(lr.regParam, np.arange(0, .1, .01))
+grid = grid.addGrid(lr.elasticNetParam,[0,1])
+
+# Build the grid
+grid = grid.build()
