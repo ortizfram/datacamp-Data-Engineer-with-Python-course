a919b6ef62e5b01840016d80593c53ddb7c9db19
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-14 13:45:30 -0900

    Create IV. Removing repetitive work.py

---
+++ Introduction to AWS Boto in Python/1. Putting Files in the Cloud!/IV. Removing repetitive work.py
@@ -0,0 +1,26 @@
+"""
+
+\  Removing repetitive work  /
+
+Automation is at the heart of data engineering. Sam wants to eliminate boring and repetitive tasks from her and her coworkers plate. 
+She wants to let machines do what they do best, while empowering humans to do what they do best.
+
+Sam has a coworker - John - whose job is to build the department budget. He also got stuck with the job of looking at images coming from GetItDone user reports.
+If he sees a cat in the image, he has to send a text message to the Animal services manager with the image and its location.
+
+Sam is thinking of building a system that would take this repetitive work off John's plate.
+
+Such a system will:
+
+Get images taken by city trash trucks and store them;
+If the image contains a cat, the system will send an SMS to the Animal Services manager.
+What AWS services would Sam need to implement this system?
+---
+
+      IAM
+      S3
+      SNS
+      Rekognition
+ANSW: All of the Above
+
+"""
