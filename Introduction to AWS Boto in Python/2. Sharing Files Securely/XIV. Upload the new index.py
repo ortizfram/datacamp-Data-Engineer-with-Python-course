d0e82f3373f68bdc580a912302dd48712af32911
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-15 13:43:23 -0300

    Create XIV. Upload the new index.py

---
+++ Introduction to AWS Boto in Python/2. Sharing Files Securely/XIV. Upload the new index.py
@@ -0,0 +1,19 @@
+"""
+Upload the new index
+Sam is almost done! In the last exercise, she generated a new directory listing, storing it in the objects_df variable:
+
+Screenshot of objects_df
+
+Sam has created the boto3 S3 client in the s3 variable. objects_df is populated with the new directory listing from the previous exercise.
+
+The next step is to write objects_df to an HTML file, and upload it to S3 replacing the current 'index.html' file.
+
+Help Sam update the directory listing, letting the public access reports for February as well as January!
+
+Instructions
+100 XP
+_ Write objects_df to an HTML file 'report_listing.html' with clickable links.
+_ The HTML file should only contain 'Link', 'LastModified', and 'Size' columns.
+_ Overwrite the 'index.html' on S3 by uploading the new version of the file.
+
+"""
