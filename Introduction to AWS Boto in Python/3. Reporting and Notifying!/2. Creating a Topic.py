852ea129b80aed25f8fda5bf853ee083421fb0aa
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-15 11:25:01 -0900

    Update 2. Creating a Topic.py

--- Introduction to AWS Boto in Python/3. Reporting and Notifying!/2. Creating a Topic.py
+++ Introduction to AWS Boto in Python/3. Reporting and Notifying!/2. Creating a Topic.py
@@ -18,3 +18,18 @@ _ Re-create the 'city_alerts' topic and extract its topic ARN with a one-liner.
 _ Verify the two topic ARNs match.
 
 """
+# Initialize boto3 client for SNS
+sns = boto3.client('sns', 
+                   region_name='us-east-1', 
+                   aws_access_key_id=AWS_KEY_ID, 
+                   aws_secret_access_key=AWS_SECRET)
+
+# Create the city_alerts topic
+response = sns.create_topic(Name="city_alerts")
+c_alerts_arn = response['TopicArn']
+
+# Re-create the city_alerts topic using a oneliner
+c_alerts_arn_1 = sns.create_topic(Name='city_alerts')['TopicArn']
+
+# Compare the two to make sure they match
+print(c_alerts_arn == c_alerts_arn_1)
