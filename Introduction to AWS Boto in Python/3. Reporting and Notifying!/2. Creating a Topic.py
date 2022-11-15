e4e18278810482d2ae486539194d0e1913a0c5ad
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-15 11:21:10 -0900

    Create 2. Creating a Topic.py

---
+++ Introduction to AWS Boto in Python/3. Reporting and Notifying!/2. Creating a Topic.py
@@ -0,0 +1,20 @@
+"""
+Creating a Topic
+Sam has been doing such a great job with her new skills, she got a promotion.
+
+With her new title of Information Systems Analyst 3 (as opposed to 2), she has gotten a tiny pay bump in exchange for a lot more work.
+
+Knowing what she can do, the City Council asked Sam to prototype an alerting system that will alert them when any department has more than 100 Get It Done requests outstanding.
+
+They would like for council members and department directors to receive the alert.
+
+Help Sam use her new knowledge of Amazon SNS to create an alerting system for Council!
+
+Instructions
+100 XP
+_ Initialize the boto3 client for SNS.
+_ Create the 'city_alerts' topic and extract its topic ARN.
+_ Re-create the 'city_alerts' topic and extract its topic ARN with a one-liner.
+_ Verify the two topic ARNs match.
+
+"""
