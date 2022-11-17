a9ab1dfdd03870ec0d3cda86b6445836a6019bcf
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-17 08:35:26 -0300

    Create 7. Delete tables with DROP TABLE.py

---
+++ Introduction to Relational Databases in SQL/7. Delete tables with DROP TABLE.py
@@ -0,0 +1,14 @@
+"""
+Delete tables with DROP TABLE
+Obviously, the university_professors table is now no longer needed and can safely be deleted.
+
+For table deletion, you can use the simple command:
+
+DROP TABLE table_name;
+Instructions
+100 XP
+Delete the university_professors table.
+
+"""
+#-- Delete the university_professors table
+DROP TABLE university_professors;
