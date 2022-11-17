e2caf85a37549debc6374546117c309e1ce57ad8
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-17 08:55:16 -0300

    Create 2. Types of database constraints.py

---
+++ Introduction to Relational Databases in SQL/2. Enforce data consistency with attribute constraints/2. Types of database constraints.py
@@ -0,0 +1,14 @@
+"""
+Types of database constraints
+Which of the following is not used to enforce a database constraint?
+
+Answer the question
+50XP
+Possible Answers
+
+          Foreign keys
+    OK    SQL aggregate functions  # only does calculations
+          The BIGINT data type
+          Primary keys
+
+"""
