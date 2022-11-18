00b0578372e3b29ee04d09524ee34f3e15cbad53
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-18 15:05:30 -0300

    Create 5. .py

---
+++ Database Design/1. Processing, Storing, and Organizing Data/5. .py
@@ -0,0 +1,15 @@
+"""
+Recommend a storage solution
+----When should you choose a data warehouse over a data lake?
+
+Answer the question
+50XP
+Possible Answers
+
+        - To train a machine learning model with a 150 GB of raw image data.            # data lake because the data is large and unstructured.
+        - To store real-time social media posts that may be used for future analysis    # Data warehouses cause will be used for analysis.
+        - To store customer data that needs to be updated regularly                     # Data warehouses are usually read-only
+     OK - To create accessible and isolated data repositories for other analysts        #  Analysts appreciate working in a data warehouse more because of its ..>
+                                                                                            organization of structured data that make analysis easier.  
+
+"""
