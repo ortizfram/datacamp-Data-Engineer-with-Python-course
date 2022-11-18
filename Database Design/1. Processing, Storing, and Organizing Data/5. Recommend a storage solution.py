a01ad115ee8d0b19304be948991ca4e4ec1df170
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-18 15:06:52 -0300

    Update 5. Recommend a storage solution.py

--- Database Design/1. Processing, Storing, and Organizing Data/5. Recommend a storage solution.py
+++ Database Design/1. Processing, Storing, and Organizing Data/5. Recommend a storage solution.py
@@ -8,7 +8,7 @@ Possible Answers
 
         - To train a machine learning model with a 150 GB of raw image data.            # data lake because the data is large and unstructured.
         - To store real-time social media posts that may be used for future analysis    # Data warehouses cause will be used for analysis.
-        - To store customer data that needs to be updated regularly                     # Data warehouses are usually read-only
+        - To store customer data that needs to be updated regularly                     # Data warehouses -> read-only
      OK - To create accessible and isolated data repositories for other analysts        #  Analysts appreciate working in a data warehouse more because of its ..>
                                                                                             organization of structured data that make analysis easier.  
 
