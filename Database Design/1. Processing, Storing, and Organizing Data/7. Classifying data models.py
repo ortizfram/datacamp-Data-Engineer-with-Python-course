e5527bd04c4339ac85a74240f2ca5fe32ce55a57
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-18 11:14:21 -0900

    Create 7. Classifying data models.py

---
+++ Database Design/1. Processing, Storing, and Organizing Data/7. Classifying data models.py
@@ -0,0 +1,21 @@
+"""
+Classifying data models
+In the previous video, we learned about three different levels of data models: conceptual, logical, and physical.
+
+Instructions
+100XP
+- Each of these cards hold a tool or concept that fits into a certain type of data model. Place the cards in the correct category.
+
+"""
+'''
+ @ Conceptual Data Model
+  . Gathers business requirements
+  . Entities, attributes, and relationships
+  
+@ Logical Data Model
+  . Relational model
+  . Determining tables and columns
+  
+@ Physical Data Model
+  . File structure of data storage
+  '''
