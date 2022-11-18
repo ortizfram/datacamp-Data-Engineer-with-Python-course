ac8fa32e4fdd8adaf7729a227fe723eb3b4ac9b9
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-11-18 14:53:42 -0300

    Create 3. Name that data type.py

---
+++ Database Design/1. Processing, Storing, and Organizing Data/3. Name that data type.py
@@ -0,0 +1,25 @@
+"""
+Name that data type!
+In the previous video, you learned about structured, semi-structured, and unstructured data. Structured data is the easiest to analyze because it is organized and cleaned. On the other hand, unstructured data is schemaless, but scales well. In the middle we have semi-structured data for everything in between.
+
+Instructions
+100XP
+Each of these cards hold a type of data. Place them in the correct category.
+
+"""
+'''
+-> Unstructured
+   - Zip fle of all text messages ever recelved
+   - Images In your photo library
+   - To-do notes In a text editor
+   
+-> Semi-structured:
+   - JSON oblect of tweets outputted In real-time by the Twitter API
+   - CSVs of open data downloaded from your local government websltes
+   - HTML sytnax
+   
+-> Structured:
+   - A relational database with latest withdrawals
+      and deposits made by clients
+      
+'''
